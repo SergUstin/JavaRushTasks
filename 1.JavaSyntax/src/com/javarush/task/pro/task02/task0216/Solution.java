b8b7687d750859520659b866990288a95a62219e
SergUstin <btboy_80@mail.ru>
2022-12-12 13:50:11 +0300

    add solution javaSyntax task0216

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Serg";
+        String date = "сегодня";
+        String time = "в 4 часа";
+        String adjective = "вычислительный";
+        String noun = "механизм";
+        String verb = "обрабатывает";
+
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
