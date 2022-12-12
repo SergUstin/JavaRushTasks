f01a001ad8d4ceb06060db94ec1de1f8b9328125
SergUstin <btboy_80@mail.ru>
2022-12-12 12:59:13 +0300

    add solution javaSyntax task0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 2, age = 20;
+        String documents = "Документы";
+        String behavior = "Виновен";
+    }
+}
