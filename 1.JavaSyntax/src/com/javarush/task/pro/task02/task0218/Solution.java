5efb5fd887c2e5a7ccb71fe05d333ea16944b3ab
SergUstin <btboy_80@mail.ru>
2022-12-12 16:09:15 +0300

    add solution javaSyntax task0218

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0218/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0218;
+
+import java.util.Scanner;
+
+/* 
+Ручное управление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+
+        System.out.println(a * b * c);
+
+    }
+}
