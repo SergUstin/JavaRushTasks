92fc40478ff58832e7f1afb2735d6bbf6cee9ba9
SergUstin <btboy_80@mail.ru>
2022-12-19 13:34:42 +0300

    add solution javaSyntax task0405

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0405;
+
+/* 
+Незаполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int n = 0;
+        String space = " ";
+
+        while (n < 10) {
+            int m = 0;
+            while (m < 20) {
+                m++;
+            }
+            System.out.println();
+            n++;
+        }
+    }
+}
