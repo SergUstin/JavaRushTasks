eb9e803c297ce2326d3041cc4af15adf9cbffcb4
SergUstin <btboy_80@mail.ru>
2022-12-19 22:04:31 +0300

    add solution javaSyntax task0420

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0420/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0420;
+
+import java.util.Scanner;
+
+/* 
+Страшное число
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Введите предполагаемое число");
+
+        checkNumber(3);
+    }
+
+    public static void checkNumber(int number) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber;
+        //напишите тут ваш код
+    }
+}
