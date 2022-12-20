ca3c0521f0a6a9c1e888e7806bef66a3d29bd0ee
SergUstin <btboy_80@mail.ru>
2022-12-20 10:34:45 +0300

    add solution javaSyntax task0403

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -9,33 +9,21 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String stop = scanner.nextLine();
-        int count = scanner.nextInt();
         int sum = 0;
-        String text = "ENTER";
 
-        while (scanner.hasNextInt()) {
-            sum += count;
-        }
-
-        if (stop.equalsIgnoreCase(text)) {
-            System.out.println(sum);
-        }
-
-
-
-
-
-
-
-        /*if (!stop.equalsIgnoreCase("ENTER")) {
-            sum += count;
-            System.out.println(sum);
-        } else {
-            while (!stop.equalsIgnoreCase("ENTER")) {
-                count++;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                sum += number;
+            } else if (scanner.hasNextLine()) {
+                String str = scanner.nextLine();
+                if (str.equalsIgnoreCase("ENTER")) {
+                    break;
+                }
             }
-        }*/
+        }
+
+        System.out.println(sum);
 
 
     }
