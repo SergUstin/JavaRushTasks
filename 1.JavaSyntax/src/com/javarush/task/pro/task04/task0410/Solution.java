ae171bd67cef555d1257f58f3d7a9348b09d4ccd
SergUstin <btboy_80@mail.ru>
2022-12-20 12:56:09 +0300

    add solution javaSyntax task0410

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
@@ -8,7 +8,21 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int min = Integer.MIN_VALUE;
+        int secondMin = 0;
 
+        while (scanner.hasNextInt()) {
+            int num = scanner.nextInt();
+            if (min < num) {
+                min = num;
+            } else if (secondMin < num) {
+                secondMin = num;
+                //if (min < secondMin)
+            }
+        }
+        System.out.println(min);
+        System.out.println(secondMin);
 
     }
 }
