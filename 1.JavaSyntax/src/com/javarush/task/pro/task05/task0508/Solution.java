b75f1c58e0a5ab16478683fbac7f77d31bdb0406
SergUstin <btboy_80@mail.ru>
2022-12-22 11:05:53 +0300

    add solution javaSyntax task0508

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task05.task0508;
+
+import java.util.Scanner;
+
+/* 
+Удаляем одинаковые строки
+*/
+
+public class Solution {
+    public static String[] strings;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        strings = new String[6];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            String str = strings[i];
+            for (int j = i + 1; j < strings.length; j++) {
+                if (str == null) {
+                    break;
+                }
+                if (str.equalsIgnoreCase(strings[j])) {
+                    strings[i] = null;
+                    strings[j] = null;
+                }
+            }
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ", ");
+        }
+    }
+}
