8d7affebca9a658d3821ac0b7019fb27bde78216
SergUstin <btboy_80@mail.ru>
2022-12-22 10:32:53 +0300

    add solution javaSyntax task0523

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0523/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0523/Solution.java
@@ -8,6 +8,17 @@ public class Solution {
     public static int[] coordinates = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
 
+        for (int i = 0; i < coordinates.length; i++) {
+            if (coordinates[i] % 2 != 0) {
+                x += coordinates[i];
+            } else {
+                y += coordinates[i];
+            }
+        }
+
+        System.out.println("(" + x + ";" + y + ")");
     }
 }
