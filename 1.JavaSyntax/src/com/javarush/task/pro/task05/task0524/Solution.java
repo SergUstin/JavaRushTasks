98d10bf97002d6e1cc6e615c110a81d1701d3f22
SergUstin <btboy_80@mail.ru>
2022-12-27 10:23:15 +0300

    add solution javaSyntax task0524

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0524/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task05.task0524;
+
+/* 
+Взлом Кибердракона
+*/
+
+public class Solution {
+    public static int[] coordinates;
+
+    public static void main(String[] args) {
+        coordinates = new int[200];
+        for (int i = 0; i < coordinates.length; i++) {
+            if (i % 1 == 0 && coordinates[i] % i == 0);
+
+        }
+    }
+}
