72a21ba6b9c1182d014264f32d72c736dc0039ae
SergUstin <btboy_80@mail.ru>
2022-12-29 11:56:45 +0300

    add solution javaSyntax task0608

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(cube(3));
+
+    }
+
+    public static long cube(long a) {
+        return a * a * a;
+    }
+}
