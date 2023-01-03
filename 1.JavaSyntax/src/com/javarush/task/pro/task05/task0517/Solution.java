0540f8b669c782f4795b9a90c0fc25c4949607e2
SergUstin <btboy_80@mail.ru>
2023-01-03 11:01:10 +0300

    add solution javaSyntax task0517

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0517/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task05.task0517;
+
+import java.util.Arrays;
+
+/* 
+Делим массив
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[2][];
+    public static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(Arrays.deepToString(result));
+    }
+}
