9aa5ad5d5bcb770219e7a4da36625f7acb55455f
SergUstin <btboy_80@mail.ru>
2023-01-09 18:10:21 +0300

    add solution javaSyntax task0704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0704;
+
+/* 
+Экспонентная запись числа
+*/
+
+public class Solution {
+    double earthDiameter = 12742.0;
+    double lightSpeed = 299792458.0;
+    double uraniumAtomicMass = 238.0289;
+    double averageBeeWeight = 0.085;
+    double javaDeveloperSalary = 10000.0;
+}
