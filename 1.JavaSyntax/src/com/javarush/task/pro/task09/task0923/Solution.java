29ad70b4da8f9b48dbe4744963cd1a33d38ecb09
SergUstin <btboy_80@mail.ru>
2023-01-24 15:27:44 +0300

    add solution javaSyntax task0923

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0923/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task09.task0923;
+
+/* 
+Ранг доступа — Пиратский Капитан
+*/
+
+import static com.javarush.task.pro.task09.task0923.CaptainBridge.checkAccess;
+
+public class Solution {
+    public static void main(String[] args) {
+        checkAccess("Стив");
+        //checkAccess("Боец");
+    }
+}
