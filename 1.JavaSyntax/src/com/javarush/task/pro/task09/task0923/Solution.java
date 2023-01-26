1ceb4d4d745e169ebff339b6ec63c4ec012441aa
SergUstin <btboy_80@mail.ru>
2023-01-26 12:54:18 +0300

    add solution javaSyntax task0923

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0923/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0923/Solution.java
@@ -8,7 +8,16 @@ import static com.javarush.task.pro.task09.task0923.CaptainBridge.checkAccess;
 
 public class Solution {
     public static void main(String[] args) {
-        checkAccess("Стив");
-        //checkAccess("Боец");
+        String[] ranks = {"Раб", "Гость", "Боец", "Ветеран", "Картографист", "Капитан", "Стив"};
+        String name = "";
+        for (int i = 0; i < ranks.length; i++) {
+            CaptainBridge.debugAccess(ranks[i]);
+            if (CaptainBridge.debugAccess(ranks[i])) {
+                name = ranks[i];
+            }
+        }
+
+        checkAccess(name);
+
     }
 }
