7a4b54f40c53e76d48f4c7c3c26a1bb89de21dad
SergUstin <btboy_80@mail.ru>
2023-01-30 14:35:24 +0300

    add solution javaSyntax task0925

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0925/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task09.task0925;
+
+/* 
+Освобождение Пиратов
+*/
+
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static String[] controlUnits = {"iAm/A/spyware/one", "iAm/A/malware/two", "iAm/A/virus/three", "iAm/A/trojan/four", "iAm/bug/five"};
+
+    public static void main(String[] args) {
+        splitArrayElements();
+        getStringFromArray();
+        printArray(controlUnits);
+    }
+
+    public static void splitArrayElements() {
+
+
+    }
+
+    public static void getStringFromArray() {
+        //напишите тут ваш код
+
+    }
+
+    public static void printArray(String[] array) {
+        for (String s : array) {
+            System.out.println(s);
+        }
+    }
+}
