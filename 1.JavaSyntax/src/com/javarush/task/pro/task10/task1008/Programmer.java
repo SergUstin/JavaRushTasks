ea061fb694b669fbf4472ab1f7992057f826d4ae
SergUstin <btboy_80@mail.ru>
2023-02-01 13:50:23 +0300

    add solution javaSyntax task1008

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1008/Programmer.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task10.task1008;
+
+/* 
+Зарплата
+*/
+
+public class Programmer {
+    private int salary = 1000;
+
+    public Programmer () {}
+
+    public int getSalary () {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if (salary > this.salary) {
+            this.salary = salary;
+        } else {
+            return;
+        }
+    }
+}
