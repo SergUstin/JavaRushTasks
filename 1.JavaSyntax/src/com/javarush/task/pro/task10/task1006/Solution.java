905b0405a89bb33d1eea2b3716d35267b08fc72f
SergUstin <btboy_80@mail.ru>
2023-02-01 13:35:42 +0300

    add solution javaSyntax task1006

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new ElectricCar();
+        new GasCar();
+        new HybridCar();
+    }
+}
