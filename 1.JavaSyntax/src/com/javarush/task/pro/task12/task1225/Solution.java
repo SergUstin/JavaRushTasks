b4d90d3fc2c1885b0214707644d1be539c101516
SergUstin <btboy_80@mail.ru>
2023-02-16 10:51:47 +0300

    add solution javaSyntax task1225

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1225/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1225/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.pro.task12.task1225;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 /* 
 Аккаунт ИнстаМатрицы
@@ -13,25 +14,21 @@ public class Solution {
 
     public static void main(String[] args) {
         convertAccounts();
-        System.out.println(accountsList);
         registerAccount("Амиго");
         registerAccount("Диего");
-       // System.out.println(accountsList);
-    }
 
-    public static void convertAccounts() {
-        for (int i = 0; i < Accounts.getAccounts().length; i++) {
-            accountsList.add(Accounts.getAccounts()[i]);
-            //i--;
+        for (String string : accountsList) {
+            System.out.println(string);
         }
     }
 
+    public static void convertAccounts() {
+        Collections.addAll(accountsList, Accounts.getAccounts());
+    }
+
     public static void registerAccount(String username) {
-        for (int i = 0; i < accountsList.size(); i++) {
-            if (!accountsList.get(i).equals(username)) {
-                accountsList.add(username);
-                i--;
-            }
+        if (!accountsList.contains(username)) {
+            accountsList.add(username);
         }
     }
 }
