4f72b2e6e79150f51fb083a0d89619479e115c6f
SergUstin <btboy_80@mail.ru>
2023-02-23 10:31:11 +0300

    add solution javaSyntax task1314

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1314/StringLinkedList.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task13.task1314;
+
+public class StringLinkedList {
+    private Node first = new Node();
+    private Node last = new Node();
+
+    public void add(String value) {
+        if (first.next == null) {
+            Node node = new Node();
+            node.value = value;
+            first.next = node;
+        }
+        if (last.prev == null) {
+            last.prev = first.next;
+            return;
+        }
+
+        Node node = new Node();
+        node.value = value;
+
+        Node lastNode = last.prev;
+        lastNode.next = node;
+        node.prev = lastNode;
+        last.prev = node;
+    }
+
+    public String get(int index) {
+        int counter = 0;
+        Node node = first.next;
+        while (node != null) {
+            if (counter == index) {
+                return node.value;
+            }
+            node = node.next;
+            counter++;
+        }
+        return null;
+    }
+
+    public static class Node {
+        private Node prev;
+        private String value;
+        private Node next;
+    }
+}
