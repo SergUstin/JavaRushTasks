f632fef18764745a2bd36da5a785edc2a1b48133
SergUstin <btboy_80@mail.ru>
2023-03-02 10:51:41 +0300

    add solution javaSyntax task1504

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.pro.task15.task1504;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,13 +14,20 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        String src = "C:\\MyProjects\\JavaRushTasks\\1.JavaSyntax\\src\\com\\javarush\\task\\pro\\task15\\task1504\\File1.txt";
-        String dest = "C:\\MyProjects\\JavaRushTasks\\1.JavaSyntax\\src\\com\\javarush\\task\\pro\\task15\\task1504\\File2.txt";
-
-        try (InputStream input = Files.newInputStream(Path.of(src));
-             OutputStream output = Files.newOutputStream(Path.of(dest))) {
-
-
+        Scanner scanner = new Scanner(System.in);
+        try (InputStream input = Files.newInputStream(Path.of(scanner.nextLine()));
+            OutputStream output = Files.newOutputStream(Path.of(scanner.nextLine()))) {
+            byte[] byteIn = input.readAllBytes();
+            byte[] byteOut = new byte[byteIn.length];
+            for (int i = 0; i < byteIn.length; i += 2) {
+                if (i < byteIn.length - 1) {
+                    byteOut[i] = byteIn[i + 1];
+                    byteOut[i + 1] = byteIn[i];
+                } else {
+                    byteOut[i] = byteIn[i];
+                }
+            }
+            output.write(byteOut);
         }
     }
 }
