fa7f87da4c16271a7f6cd6999ec98813afcb065f
SergUstin <btboy_80@mail.ru>
2023-03-03 17:00:33 +0300

    add solution javaSyntax task1520

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1520/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task15.task1520;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Перемещение файлов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path sourceDirectory = Path.of(scanner.nextLine());
+        Path targetDirectory = Path.of(scanner.nextLine());
+        //напишите тут ваш код
+    }
+}
+
