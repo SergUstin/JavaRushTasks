a3fa4961002b9a5ebb4fcfcc453bb20dd49e4b61
SergUstin <btboy_80@mail.ru>
2023-03-07 15:27:29 +0300

    add solution javaSyntax task1604

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1604/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task16.task1604;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/* 
+День недели рождения твоего
+*/
+
+public class Solution {
+
+    static Calendar birthDate = new GregorianCalendar(1992, Calendar.MAY, 8);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Calendar calendar) {
+        String str = "";
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (day) {
+            case 1:
+                return "воскресенье";
+            case 2:
+                return "понедельник";
+            case 3:
+                return "вторник";
+            case 4:
+                return "среда";
+            case 5:
+                return "четверг";
+            case 6:
+                return "пятница";
+            case 7:
+                return "суббота";
+        }
+        return str;
+    }
+}
