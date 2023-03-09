841baaf341eb5f195d1f7475ea93fd870d3e1ca4
SergUstin <btboy_80@mail.ru>
2023-03-09 15:31:10 +0300

    add solution javaSyntax task1607

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1607/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.pro.task16.task1607;
+
+import java.time.LocalDate;
+
+/* 
+Освоение нового API
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(nowExample());
+        System.out.println(ofExample());
+        System.out.println(ofYearDayExample());
+        System.out.println(ofEpochDayExample());
+    }
+
+    static LocalDate nowExample() {
+        //напишите тут ваш код
+
+        return null;
+    }
+
+    static LocalDate ofExample() {
+        //напишите тут ваш код
+
+        return null;
+    }
+
+    static LocalDate ofYearDayExample() {
+        //напишите тут ваш код
+
+        return null;
+    }
+
+    static LocalDate ofEpochDayExample() {
+        //напишите тут ваш код
+
+        return null;
+    }
+}
