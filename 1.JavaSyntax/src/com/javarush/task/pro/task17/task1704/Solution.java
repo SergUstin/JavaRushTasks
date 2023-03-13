e1f465735e56ea4a8027851b133654df7f2230e3
SergUstin <btboy_80@mail.ru>
2023-03-13 11:36:16 +0300

    add solution javaSyntax task1704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1704/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.task.pro.task17.task1704;
+
+import java.util.ArrayList;
+
+/* 
+Космическая одиссея ч.2
+*/
+
+public class Solution {
+    public static ArrayList<Astronaut> astronauts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createCrew();
+        printCrewInfo();
+        runWorkingProcess();
+    }
+
+    public static void runWorkingProcess() {
+        for (Astronaut astronaut : astronauts) {
+            int index = astronauts.indexOf(astronaut);
+            if (index <= 1) {
+                pilot((Human) astronaut);
+            } else if (index == 2) {
+                createDirection((Dog) astronaut);
+            } else {
+                research((Cat) astronaut);
+            }
+        }
+    }
+
+    public static void pilot(Human human){
+        System.out.println("Член экипажа " + human.getInfo() + " пилотирует корабль.");
+    }
+
+    public static void createDirection(Dog dog){
+        System.out.println("Член экипажа " + dog.getInfo() + " занимается созданием навигационного маршрута.");
+    }
+
+    public static void research(Cat cat){
+        System.out.println("Член экипажа " + cat.getInfo() + " исследует ближайшие планеты.");
+    }
+
+    public static void createCrew() {
+        astronauts.add(new Human());
+        astronauts.add(new Human());
+        astronauts.add(new Dog());
+        astronauts.add(new Cat());
+    }
+
+    public static void printCrewInfo() {
+        System.out.println("На борт погружены члены экипажа: ");
+        for (Astronaut astronaut : astronauts) {
+            System.out.println(astronaut.getInfo());
+        }
+    }
+}
