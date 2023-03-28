918e275dfff60199957a1bc37f1549c8a134b318
SergUstin <btboy_80@mail.ru>
2023-03-28 18:01:49 +0300

    add solution javaCore task1227

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1227/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task12.task1227;
+
+/* 
+CanFly, CanRun, CanSwim для классов Duck, Penguin, Toad
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+
+    public class Duck implements CanRun, CanFly, CanSwim {
+
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Penguin implements CanSwim, CanRun {
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Toad implements CanSwim {
+
+        @Override
+        public void swim() {
+
+        }
+    }
+}
