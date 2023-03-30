7bfbc6a236977ef8eb680e589a0e34cac5071b0a
SergUstin <btboy_80@mail.ru>
2023-03-30 11:50:52 +0300

    add solution javaCore task1824

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1824/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task18.task1824;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/* 
+Из потока данных — в map
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("JavaRush", "CodeGym", "Amigo", "Elly", "Kim", "Risha");
+
+        getMap(stringStream).forEach((s, i) -> System.out.println(s + " - " + i));
+
+    }
+
+    public static Map<String, Integer> getMap(Stream<String> stringStream) {
+        return stringStream.collect(Collectors.toMap(e -> e, String::length));
+    }
+}
