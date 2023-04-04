ece74372cf0d1ddafa7640f53a7514920229cac8
SergUstin <btboy_80@mail.ru>
2023-04-04 12:09:46 +0300

    add solution javaCore task1319

--- 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -1,25 +1,27 @@
 package com.javarush.task.task13.task1319;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 
 /* 
 Писатель в файл с консоли
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        String sours = reader.readLine();
-        FileInputStream fileInputStream = new FileInputStream(sours);
+    public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(fileInputStream);
-        StringBuilder builder = new StringBuilder();
-
-        while (scanner.hasNext("exit")) {
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(reader.readLine()))) {
+            while (true) {
+                String str = reader.readLine();
+                writer.write(str);
+                writer.newLine();
+                if (str.equalsIgnoreCase("exit")) {
+                    break;
+                }
+            }
         }
-
     }
 }
