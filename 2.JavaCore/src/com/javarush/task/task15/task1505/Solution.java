04546b6c3b182b342db0bb2e10465f86926d1434
SergUstin <btboy_80@mail.ru>
2023-04-05 12:10:15 +0300

    add solution javaCore task1505

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1505/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task15.task1505;
+
+/* 
+ООП - исправь ошибки в наследовании
+*/
+
+public class Solution {
+    public interface HasWeight {
+        int getWeight();
+    }
+
+    public interface HasHeight {
+        int getHeight();
+    }
+
+    public static class Human implements HasWeight, HasHeight {
+        @Override
+        public int getWeight() {
+            return 50;
+        }
+
+        @Override
+        public int getHeight() {
+            return 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.getWeight());
+        System.out.println(human.getHeight());
+    }
+}
