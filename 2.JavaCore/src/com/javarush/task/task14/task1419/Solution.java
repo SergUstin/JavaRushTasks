aaef581dbf550dd5658e359c7357d96941351163
SergUstin <btboy_80@mail.ru>
2023-04-07 10:16:49 +0300

    add solution javaCore task1419

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task14.task1419;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Нашествие исключений
+*/
+
+public class Solution {
+    public static List<Exception> exceptions = new ArrayList<Exception>();
+
+    public static void main(String[] args) {
+        initExceptions();
+
+        for (Exception exception : exceptions) {
+            System.out.println(exception);
+        }
+    }
+
+    private static void initExceptions() {
+        try {
+            float i = 1 / 0;
+
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        exceptions.add(new ArrayIndexOutOfBoundsException());
+        exceptions.add(new IllegalArgumentException());
+        exceptions.add(new IllegalAccessException());
+        exceptions.add(new NumberFormatException());
+        exceptions.add(new ClassCastException());
+        exceptions.add(new IOException());
+        exceptions.add(new InterruptedIOException());
+        exceptions.add(new InterruptedException());
+        exceptions.add(new SecurityException());
+    }
+}
