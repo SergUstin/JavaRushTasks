5cee2d1ced2ae57779c2b7d897cd0a9df52d5d2c
SergUstin <btboy_80@mail.ru>
2023-04-20 11:52:48 +0300

    add solution javaCore task1601

--- 2.JavaCore/src/com/javarush/task/task16/task1601/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1601/Solution.java
@@ -9,4 +9,11 @@ public class Solution {
         TestThread task = new TestThread();
         new Thread(task).start();
     }
+
+    public static class TestThread implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("My first thread");
+        }
+    }
 }
