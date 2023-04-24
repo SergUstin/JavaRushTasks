fd44ea4acdd8b9aff1e57c82a04c9a0a1d0f06b2
SergUstin <btboy_80@mail.ru>
2023-04-24 15:09:39 +0300

    add solution javaCore task1531

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1531/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1531;
+
+public class SubSolution extends Solution {
+    public SubSolution() {
+    }
+
+    protected SubSolution(double i) {
+//        super(i);
+    }
+
+    SubSolution(short i) {
+//        super(i);
+    }
+}
