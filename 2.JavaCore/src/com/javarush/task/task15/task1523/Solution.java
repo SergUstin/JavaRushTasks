cf66391c2797d45406de469f528623121537a0a6
SergUstin <btboy_80@mail.ru>
2023-04-24 15:09:13 +0300

    add solution javaCore task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public Solution() {
+
+    }
+
+    private Solution(int i) {
+
+    }
+
+    protected Solution(double i) {
+
+    }
+
+    Solution(short i) {
+
+    }
+}
+
