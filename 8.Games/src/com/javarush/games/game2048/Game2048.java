e750d5f33dbe1d269b574ea1c6f47e377f86e3fa
SergUstin <btboy_80@mail.ru>
2023-05-02 16:13:41 +0300

    add solution javaSyntax Game2048 8/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -84,6 +84,18 @@ public class Game2048 extends Game {
         return result;
     }
 
+    private boolean mergeRow(int[] row) {
+        boolean result = false;
+        for (int i = 0; i < row.length - 1; i++) {
+            if (row[i] != 0 && row[i] == row[i + 1]) {
+                row[i] += row[i + 1];
+                row[i + 1] = 0;
+                result = true;
+            }
+        }
+        return result;
+    }
+
     private void drawScene() {
         for (int y = 0; y < SIDE; y++) {
             for (int x = 0; x < SIDE; x++) {
