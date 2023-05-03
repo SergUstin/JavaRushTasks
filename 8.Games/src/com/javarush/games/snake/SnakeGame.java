315fca686a90dfcdbd65e1104f810f3b0ae072b0
SergUstin <btboy_80@mail.ru>
2023-05-03 11:46:30 +0300

    add solution javaSyntax Snake 1/20

---
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -0,0 +1,13 @@
+package com.javarush.games.snake;
+
+import com.javarush.engine.cell.*;
+
+public class SnakeGame extends Game {
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+
+    @Override
+    public void initialize() {
+        setScreenSize(WIDTH, HEIGHT);
+    }
+}
