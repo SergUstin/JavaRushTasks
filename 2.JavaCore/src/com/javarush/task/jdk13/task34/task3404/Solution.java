fba458548e703117ed69502186e3fcb35857ff4c
SergUstin <btboy_80@mail.ru>
2023-05-05 11:22:39 +0300

    add solution javaCore task3404

--- 2.JavaCore/src/com/javarush/task/jdk13/task34/task3404/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3404/Solution.java
@@ -11,6 +11,13 @@ public class Solution {
     }
 
     public static void moveRing(char a, char b, char c, int numRings) {
-        //напишите тут ваш код
+        if (numRings == 1) {
+            System.out.printf("From %s to %s\n", a, b);
+        } else {
+            c = (char) (numRings - a - b);
+            moveRing(a, b, c, numRings - 1);
+            System.out.printf("From %s to %s\n", a, b, c);
+            moveRing(a, b, c, numRings);
+        }
     }
 }
