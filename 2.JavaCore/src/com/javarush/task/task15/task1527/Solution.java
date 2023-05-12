d99406b77f86029d0361a1d9ef19ce79b288a28f
SergUstin <btboy_80@mail.ru>
2023-05-12 12:18:51 +0300

    add solution javaCore task1527

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1527;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Парсер реквестов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String url = reader.readLine();
+        //напишите тут ваш код
+    }
+
+    public static void alert(double value) {
+        System.out.println("double: " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String: " + value);
+    }
+}
