bdce2574e1caf381e7eb36dca83b92cdd5dec037
SergUstin <btboy_80@mail.ru>
2023-05-13 14:51:18 +0300

    add solution javaSyntax Snake 6/20

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -1,10 +1,15 @@
 package com.javarush.games.snake;
 
+import com.javarush.engine.cell.Game;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
     private List<GameObject> snakeParts = new ArrayList<>();
+    private static final String HEAD_SIGN = "\uD83D\uDC7E";
+    private static final String BODY_SIGN = "\u26AB";
+
 
     public Snake(int x, int y) {
         GameObject gameObject1 = new GameObject(x, y);
@@ -13,5 +18,11 @@ public class Snake {
         snakeParts.add(gameObject1);
         snakeParts.add(gameObject2);
         snakeParts.add(gameObject3);
+    }
+
+    public void draw (Game game) {
+        for (GameObject snakePart : snakeParts) {
+//            snakePart = game.setCellValue();
+        }
     }
 }
