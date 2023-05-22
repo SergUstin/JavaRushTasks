50e4a71e4e1c7ea02b07612904bb5e646a1bfc62
SergUstin <btboy_80@mail.ru>
2023-05-22 15:07:00 +0300

    add solution javaCore task1808

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task18.task1808;
+
+import java.io.*;
+
+/* 
+Разделение файла
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputFile = reader.readLine();
+        String outputFile1 = reader.readLine();
+        String outputFile2 = reader.readLine();
+
+        try (FileInputStream fileInputStream = new FileInputStream(inputFile);
+             FileOutputStream fileOutputStream1 = new FileOutputStream(outputFile1);
+             FileOutputStream fileOutputStream2 = new FileOutputStream(outputFile2)) {
+
+            int halfOfFile = (fileInputStream.available() + 1) / 2;
+            int numberOfByte = 0;
+            while (fileInputStream.available() > 0) {
+                if (numberOfByte < halfOfFile) {
+                    fileOutputStream1.write(fileInputStream.read());
+                    numberOfByte++;
+                } else fileOutputStream2.write(fileInputStream.read());
+            }
+        }
+
+    }
+}
