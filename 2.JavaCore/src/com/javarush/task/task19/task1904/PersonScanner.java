9fcacbb6b3a533403989e197501afaae7e17eadc
SergUstin <btboy_80@mail.ru>
2023-05-31 10:59:48 +0300

    add solution javaCore task1904

--- 2.JavaCore/src/com/javarush/task/task19/task1904/PersonScanner.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/PersonScanner.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task19.task1904;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 public interface PersonScanner {
     Person read() throws IOException;
