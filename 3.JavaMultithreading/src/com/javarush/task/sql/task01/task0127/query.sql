f02e5b907f7ea5ff1e541e103db799b4b93ed21e
SergUstin <btboy_80@mail.ru>
2023-06-08 12:02:05 +0300

    add SQL JavaMultithreading task0127

--- 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0127/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0127/query.sql
@@ -1,4 +1,4 @@
 -- Write your code here:
 select *
 from car
-where model NOT LIKE '%a' AND (quantity - booked_quantity) between 5 and 500;
+where model NOT LIKE '%a%' AND (quantity - booked_quantity) between 5 and 500;
