fb914c426c1018c81835cb7a0766f6bd0fc5caee
SergUstin <btboy_80@mail.ru>
2023-06-08 12:02:01 +0300

    add SQL JavaMultithreading task0126

--- 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0126/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0126/query.sql
@@ -1,5 +1,5 @@
 -- Write your code here:
 select brand, model, quantity, booked_quantity
 from car
-where model IN (renault, opel, seat, skoda)
-or (quantity - booked_quantity) < 10;
+where model IN ('renault', 'opel', 'seat', 'skoda')
+or quantity - booked_quantity < 10;
