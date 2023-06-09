6fb323571bdea8a09113aeb5f4b6323e144c9738
SergUstin <btboy_80@mail.ru>
2023-06-09 16:34:13 +0300

    add solution javaCore task1920

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task19.task1920;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/* 
+Самый богатый
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
