fb2f897804b8b0f15bbcd91a613dd4a2fb0a4b2d
SergUstin <btboy_80@mail.ru>
2023-06-19 12:49:31 +0300

    add solution javaCore task1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -8,7 +8,19 @@ import java.io.IOException;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int[] count = new int[128];
+        try (FileReader reader = new FileReader(args[0])) {
+            while (reader.ready()) {
+                count[reader.read()]++;
+            }
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                System.out.println((char) i + " " + count[i]);
+            }
+        }
 
     }
 }
