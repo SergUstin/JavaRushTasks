d22bd013d93642de1ad0d4dae65ca13b858d5fac
SergUstin <btboy_80@mail.ru>
2023-06-22 12:12:06 +0300

    add SQL JavaMultithreading task0231

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0231/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0231/query.sql
@@ -1,4 +1,5 @@
 -- Write your code here:
 select department as department_name, count(*) as caunt
 from employee
-where position = 'frontend developer' or 'backend developer';
+where position = 'frontend developer' or position = 'backend developer'
+group by department;
