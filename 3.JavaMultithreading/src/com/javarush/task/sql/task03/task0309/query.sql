eb1eef373a9cd2cc22f605cf632d4ab8cecf5877
SergUstin <btboy_80@mail.ru>
2023-06-27 19:41:15 +0300

    add SQL JavaMultithreading task0309

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0309/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0309/query.sql
@@ -1,1 +1,6 @@
 -- Write your code here:
+select gym.location as gym_location,
+       person.location as person_location
+from gyms gym, customer person
+where customer.location != 'London'
+group by gym.location, person.location
