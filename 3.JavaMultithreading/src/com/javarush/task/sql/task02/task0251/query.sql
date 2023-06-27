587b1481af18ff57d3274cad8050dc19cdc8aa93
SergUstin <btboy_80@mail.ru>
2023-06-27 13:01:07 +0300

    add SQL JavaMultithreading task0251

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0251/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0251/query.sql
@@ -1,1 +1,8 @@
 -- Write your code here:
+select department, position, count(*) as total
+from employee
+where position = 'manager'
+group by department, position
+having total = 1
+limit 50
+offset 1
