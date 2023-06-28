08fc0847a3c7cce805e122b1c75c582454203056
SergUstin <btboy_80@mail.ru>
2023-06-28 14:49:30 +0300

    add SQL JavaMultithreading task0316

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0316/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0316/query.sql
@@ -1,1 +1,6 @@
 -- Write your code here:
+select retailer.name as ret_name,
+       retailer.hq_location as ret_locationret_location,
+       supplier.name as sup_name
+from top_retailers retailer, suppliers supplier
+where supplier.name > 100
