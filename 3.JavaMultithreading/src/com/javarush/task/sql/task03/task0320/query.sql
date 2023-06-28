50b5abc66a4b4e9f715401b3ca8436f62d244955
SergUstin <btboy_80@mail.ru>
2023-06-28 14:49:48 +0300

    add SQL JavaMultithreading task0320

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0320/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0320/query.sql
@@ -1,1 +1,8 @@
 -- Write your code here:
+select retailer.ret_name,
+       retailer.ret_revenue,
+       supplier.sup_name,
+       supplier.sup_revenue
+from top_retailers retailer join suppliers supplier
+    on retailer.ret_revenue = supplier.sup_revenue
+where supplier.sup_revenue > 50;
