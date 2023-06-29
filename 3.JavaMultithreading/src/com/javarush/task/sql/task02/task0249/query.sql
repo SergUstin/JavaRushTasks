6cd410bc341bf52d5402776c86c2b1fdb5465c00
SergUstin <btboy_80@mail.ru>
2023-06-29 20:19:30 +0300

    add SQL JavaMultithreading task0249

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0249/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0249/query.sql
@@ -1,6 +1,5 @@
 -- Write your code here:
 select department, position, count(*) as total
 from employee
-where postion = 'backend developer'
 group by department, position
-having total = 1
+having total = 1 and postion = 'backend developer'
