5fbc2e53eb01f1a4b4a6afb317f738d736d68200
SergUstin <btboy_80@mail.ru>
2023-06-30 18:02:21 +0300

    add SQL JavaMultithreading task0322

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0322/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0322/query.sql
@@ -1,1 +1,7 @@
 -- Write your code here:
+select c.customer_id,
+       c.email,
+       o.order_id,
+       o.order_status
+from customers c join orders o on c.customer_id = o.customer_id
+where o.order_status like '%PAID%' or o.order_status like '%PROCESSING%';
