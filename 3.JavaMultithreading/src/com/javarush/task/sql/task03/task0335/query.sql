fbedc6e86370bb8b57e33c2473bbaf3672b1ebd6
SergUstin <btboy_80@mail.ru>
2023-07-03 12:55:22 +0300

    add SQL JavaMultithreading task0335

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0335/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0335/query.sql
@@ -1,1 +1,7 @@
 -- Write your code here:
+select author.country as author_country,
+       count(book.book_id) as book_count
+from authors author join books book on author.id = book.author_id
+where book.genre = 'novel'
+group by author.country
+having count(book.book_id) > 2
