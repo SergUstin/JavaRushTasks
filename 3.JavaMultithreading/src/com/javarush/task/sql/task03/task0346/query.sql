2754ce045890ca311440c3e6082de233bfbd6686
SergUstin <btboy_80@mail.ru>
2023-07-03 12:56:18 +0300

    add SQL JavaMultithreading task0346

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0346/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0346/query.sql
@@ -1,1 +1,8 @@
 -- Write your code here:
+select *
+from films join (
+    select year_born
+    from film_directors
+    where year_born > 1940
+) director on director.id = films.director_id
+where genre = 'sci-fi'
