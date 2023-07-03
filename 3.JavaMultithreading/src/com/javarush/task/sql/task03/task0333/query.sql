47e40815ddff89f7f59a27523f37592bbad31e70
SergUstin <btboy_80@mail.ru>
2023-07-03 12:55:11 +0300

    add SQL JavaMultithreading task0333

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0333/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0333/query.sql
@@ -1,1 +1,8 @@
 -- Write your code here:
+select a.sex as author_sex,
+       a.country as author_country,
+       b.copies_sold_millions as books_sold
+from authors a join books b on a.author_id = b.author_id
+where b.copies_sold_millions > 10
+group by a.sex, a.country, b.copies_sold_millions
+order by b.copies_sold_millions DESC
