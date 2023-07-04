46fa25e6d53ee1c5552751631a097b9fd86b64a6
SergUstin <btboy_80@mail.ru>
2023-07-04 15:55:33 +0300

    add SQL JavaMultithreading task0334

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0334/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0334/query.sql
@@ -1,9 +1,9 @@
 -- Write your code here:
-select a.last_name as author,
-       a.country as author_country,
-       b.genre as book_genre
-from authors a left join books b on a.id = b.author_id
-where b.genre = 'fantasy'
-group by a.last_name, a.country, b.genre
-order by a.country ASC
+select author.last_name as author,
+       author.country as author_country,
+       book.genre as book_genre
+from author author left join book book on author.id = book.author_id
+where book.genre = 'fantasy'
+group by author, author_country, book_genre
+order by author.country ASC
 limit 5
