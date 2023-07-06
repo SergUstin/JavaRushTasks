f469bbc63785a23f78454ce52296f9925d663829
SergUstin <btboy_80@mail.ru>
2023-07-06 12:10:40 +0300

    add SQL JavaMultithreading task0344

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0344/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0344/query.sql
@@ -1,6 +1,6 @@
 -- Write your code here:
 select title
-from films left join (select last_name
+from films left join (select last_name, id
                       from film_directors
-                      where last_name like ('%Spielberg%')
+                      where last_name = 'Spielberg'
                      ) director on director.id = films.director_id
