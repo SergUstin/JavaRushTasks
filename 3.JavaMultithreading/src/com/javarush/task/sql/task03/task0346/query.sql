59ac403a29014fd61748d7ba8e61ec62609a5d6a
SergUstin <btboy_80@mail.ru>
2023-07-06 12:10:56 +0300

    add SQL JavaMultithreading task0346

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0346/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0346/query.sql
@@ -1,8 +1,8 @@
 -- Write your code here:
 select *
 from films join (
-    select year_born
+    select year_born, id
     from film_directors
-    where year_born > 1940
+    where year_born < 1940
 ) director on director.id = films.director_id
-where genre = 'sci-fi'
+where films.genre = 'sci-fi'
