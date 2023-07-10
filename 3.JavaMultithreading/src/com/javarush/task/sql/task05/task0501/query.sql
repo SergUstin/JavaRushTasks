65abfb0ff1e1d4b0e001f79b6c8d116c0f23a91c
SergUstin <btboy_80@mail.ru>
2023-07-10 17:57:13 +0300

    add SQL JavaMultithreading task0501

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0501/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0501/query.sql
@@ -1,1 +1,13 @@
 -- Write your code here:
+insert into publisher (id, name) values (
+                                        1,
+                                        'Азбука'
+    );
+insert into publisher (id, name) values (
+                                        2,
+                                        'Фолио'
+    );
+insert into publisher (id, name) values (
+                                        3,
+                                        'Wordsworth Editions'
+    );
