9a053a81a9e2127e28616c984d2836225898159e
SergUstin <btboy_80@mail.ru>
2023-07-10 17:57:18 +0300

    add SQL JavaMultithreading task0502

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0502/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0502/query.sql
@@ -1,1 +1,7 @@
 -- Write your code here:
+insert into part (number, name, weight)
+values (3004, 'Brick 1 x 2', 0.83);
+insert into part (number, name, weight)
+    values (92909, 'Technic Steering Hub for Portal Axle', 2.5);
+insert into part (number, name, weight)
+    values (19916, 'Helmet Darth Vader Type 2 Top', 0.5);
