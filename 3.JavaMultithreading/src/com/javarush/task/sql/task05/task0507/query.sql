dadbfa0e7428805f26fb343f4026fa8678acb4b3
SergUstin <btboy_80@mail.ru>
2023-07-11 11:48:15 +0300

    add SQL JavaMultithreading task0507

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
@@ -1,7 +1,8 @@
 -- Write your code here:
 update part set name = 'Technic,
 Steering Wheel Hub 3 Pin Round'
-where id = 92909;
+where number = '92909';
+
 update part set name = 'Minifigure,
 Headgear Helmet SW Darth Vader Type 2 Top'
-where id = 19916;
+where number = '19916';
