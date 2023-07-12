ece9789eecaa4491772a249c925d1a440846adff
SergUstin <btboy_80@mail.ru>
2023-07-12 14:36:23 +0300

    add SQL JavaMultithreading task0529

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
@@ -1,1 +1,6 @@
 -- Write your code here:
+select author.full_name as full_name,
+       if(publisher.name is not null, publisher.name, 0) as name
+from book
+         left join author on author.id = book.author_id
+         left join publisher on publisher.id = book.publisher_id
