47955afd2ef0c337ce8b6aab6bd6152c77f16aab
SergUstin <btboy_80@mail.ru>
2023-07-12 14:36:08 +0300

    add SQL JavaMultithreading task0526

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0526/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0526/query.sql
@@ -1,1 +1,9 @@
 -- Write your code here:
+select book.publication_year,
+       book.isbn,
+       book.title,
+       author.full_name as author,
+       publisher.name as publisher
+from book
+         left join author on author.id = book.author_id
+         left join publisher on book.publisher_id = publisher.id;
