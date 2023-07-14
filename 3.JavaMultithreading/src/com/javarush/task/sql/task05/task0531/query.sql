aa9662ea3ab7769fc1af2a9a7392be8835d49054
SergUstin <btboy_80@mail.ru>
2023-07-14 12:27:34 +0300

    add SQL JavaMultithreading task0531

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0531/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0531/query.sql
@@ -1,4 +1,11 @@
 -- Write your code here:
+# select *
+# from space.object
+# where discovery_date between '2010-01-01' and '2020-12-31';
+
 select *
-from space.object
-where discovery_date between '2010-01-01' and '2020-12-31';
+from object
+where year(discovery_date) >= 2010
+  and month(discovery_date) >= 1
+  and year(discovery_date) <= 2020
+  and month(discovery_date) <= 12;
