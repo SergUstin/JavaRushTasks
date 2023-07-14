0a13e368626b315d3fd9cfda76257883ee4c536e
SergUstin <btboy_80@mail.ru>
2023-07-14 12:27:29 +0300

    add SQL JavaMultithreading task0529

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
@@ -1,6 +1,9 @@
 -- Write your code here:
 select author.full_name as full_name,
-       if(publisher.name is not null, publisher.name, 0) as name
+       count(distinct publisher.name) as publishers
 from book
          left join author on author.id = book.author_id
          left join publisher on publisher.id = book.publisher_id
+group by author.id;
+
+
