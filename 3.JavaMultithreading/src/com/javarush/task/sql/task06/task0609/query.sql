8e214157834805feb4af01a82fb939f49f2bcb03
SergUstin <btboy_80@mail.ru>
2023-07-17 14:45:43 +0300

    add SQL JavaMultithreading task0609

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0609/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0609/query.sql
@@ -1,1 +1,9 @@
 -- Write your code here:
+insert into sale_addresses (street, city, state, zip_code, country)
+select customers.street,
+       customers.city,
+       customers.state,
+       customers.zip_code,
+       customers.country
+from customers
+where customers.country != 'Testostan';
