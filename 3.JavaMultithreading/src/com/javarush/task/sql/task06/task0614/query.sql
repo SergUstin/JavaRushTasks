14c976691a18e539a293a33c0ddb3c722044ae18
SergUstin <btboy_80@mail.ru>
2023-07-17 14:46:12 +0300

    add SQL JavaMultithreading task0614

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0614/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0614/query.sql
@@ -1,1 +1,8 @@
 -- Write your code here:
+update employee,
+    (select count(*)
+     from task
+     where exp_date < '2022-10-01' and просроченные_задачи = 'нет') as good_emps
+set
+    salary = salary + 1000
+where employee.id = task.employee_id
