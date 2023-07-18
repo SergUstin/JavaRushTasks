051b733f4e93abf04789c18960baf855bd0a2127
SergUstin <btboy_80@mail.ru>
2023-07-18 16:31:54 +0300

    add JDBC JavaMultithreading task0703

--- 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0703/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0703/Solution.java
@@ -9,7 +9,14 @@ task0703
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test",
+                "root", "root");
+
+        Statement statement = connection.createStatement();
+        statement.executeQuery("select * from employee");
+
+        connection.close();
+        statement.close();
 
     }
 }
