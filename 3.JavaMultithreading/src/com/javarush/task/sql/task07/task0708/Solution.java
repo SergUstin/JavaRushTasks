04f6e17d46a4f2e01d701c948528987780cb5edb
SergUstin <btboy_80@mail.ru>
2023-07-18 16:32:19 +0300

    add JDBC JavaMultithreading task0708

--- 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0708/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0708/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.sql.task07.task0708;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /* 
 Получение даты
@@ -9,7 +11,22 @@ import java.sql.*;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test",
+                "root", "root");
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select name, created from employee");
+
+        while (resultSet.next()) {
+            String name = resultSet.getNString("name");
+            LocalDateTime date = resultSet.getObject("created", LocalDateTime.class);
+            System.out.println(name + " " + date);
+        }
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+
 
     }
 }
