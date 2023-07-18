d9a454e6e7c81e0cac80ca6baacf83bce783df5e
SergUstin <btboy_80@mail.ru>
2023-07-18 18:34:40 +0300

    add JDBC JavaMultithreading task0801

--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0801/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0801/Solution.java
@@ -9,7 +9,18 @@ import java.sql.*;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test",
+                "root", "root");
+
+        connection.setAutoCommit(false);
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("UPDATE employee set salary = salary + 2000 where name = 'Diego'");
+        statement.executeUpdate("UPDATE employee set salary = salary + 500 where name = 'Amigo'");
+
+        connection.commit();
+
+        statement.close();
+        connection.close();
 
     }
 }
