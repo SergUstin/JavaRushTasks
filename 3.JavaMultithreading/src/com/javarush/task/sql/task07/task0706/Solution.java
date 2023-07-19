4d5d299d9ccdede9a5f280d0217b9e55f8c8e142
SergUstin <btboy_80@mail.ru>
2023-07-19 12:07:07 +0300

    add JDBC JavaMultithreading task0706

--- 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0706/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0706/Solution.java
@@ -9,24 +9,22 @@ task0706
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test",
-                "root", "root");
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test", "root", "root");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("select * from employee limit 1")) {
 
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from employee limit 1");
-        ResultSetMetaData metaData = resultSet.getMetaData();
+            ResultSetMetaData metaData = resultSet.getMetaData();
 
-        int columnCount = metaData.getColumnCount();
+            int columnCount = metaData.getColumnCount();
 
-        for (int i = 1; i <= columnCount ; i++) {
-            String nameColumn = metaData.getColumnName(i);
-            String typeColumn = metaData.getColumnTypeName(i);
-            System.out.println(nameColumn + "(" + typeColumn + ")");
+            for (int i = 1; i <= columnCount; i++) {
+                String nameColumn = metaData.getColumnName(i);
+                String typeColumn = metaData.getColumnTypeName(i);
+                System.out.println(nameColumn + "(" + typeColumn + ")");
+            }
+
         }
-
-        resultSet.close(); // надо ли???
-        statement.close();
-        connection.close();
 
 
     }
