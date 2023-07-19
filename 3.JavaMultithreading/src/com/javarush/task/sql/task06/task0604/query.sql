1e6dd038f65605920e4a77073ea1911a11257c6f
SergUstin <btboy_80@mail.ru>
2023-07-19 12:05:59 +0300

    add SQL JavaMultithreading task0604

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0604/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0604/query.sql
@@ -1,6 +1,7 @@
 -- Write your code here:
 create table if not exists users (
-    user_id int auto_increment primary key not null,
+    user_id int auto_increment not null,
     first_name varchar(255) not null,
-    last_name varchar(255) not null
+    last_name varchar(255) not null,
+    primary key (user_id)
 );
