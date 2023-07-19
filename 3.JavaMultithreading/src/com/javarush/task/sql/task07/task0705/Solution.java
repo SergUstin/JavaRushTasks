23b7208f87683a0dc48ea9ce398b0eb960940bc4
SergUstin <btboy_80@mail.ru>
2023-07-19 12:07:00 +0300

    add JDBC JavaMultithreading task0705

--- 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0705/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0705/Solution.java
@@ -9,21 +9,15 @@ task0705
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test",
-                "root", "root");
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test", "root", "root");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("select min(distinct age) from employee")) {
 
-        Statement statement = connection.createStatement();
-
-
-        while (statement.executeQuery("select min(age) from employee").next()) {
-            int num = statement.executeQuery("select min(age) from employee")
-                    .getInt("age");
-
-            System.out.println(num);
+            while (resultSet.next()) {
+                int num = resultSet.getInt(1);
+                System.out.println(num);
+            }
         }
-
-        connection.close();
-        statement.close();
-
     }
 }
