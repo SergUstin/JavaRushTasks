c58100f598ae79b1ef655accd3273f8d595a7585
SergUstin <btboy_80@mail.ru>
2023-07-20 12:24:18 +0300

    add solution javaCore task1823

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -14,14 +14,41 @@ import java.util.Map;
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String readString;
+        while (!(readString = reader.readLine()).equals("exit")) {
+            new ReadThread(readString).start();
+        }
     }
 
     public static class ReadThread extends Thread {
+        private String fileName;
+
         public ReadThread(String fileName) {
-            //implement constructor body
+            this.fileName = fileName;
         }
-        // implement file reading here - реализуйте чтение из файла тут
+
+        @Override
+        public void run() {
+            byte[] bytesCount = new byte[256];
+            try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+                while (fileInputStream.available() > 0) {
+                    int aByte = fileInputStream.read();
+                    bytesCount[aByte]++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int maxCount = 0;
+            int maxCountByte = 0;
+            for (int i = 0; i < bytesCount.length; i++) {
+                if (bytesCount[i] > maxCount) {
+                    maxCount = bytesCount[i];
+                    maxCountByte = i;
+                }
+            }
+            resultMap.put(fileName, maxCountByte);
+        }
     }
 }
