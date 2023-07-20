f34cd5546c70b50e66010638971f644f6687f269
SergUstin <btboy_80@mail.ru>
2023-07-20 12:24:33 +0300

    add solution javaCore task1922

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -21,6 +21,31 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        String file1 = null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            file1 = reader.readLine();
 
+        } catch (IOException ignore) {
+            /* NOP */
+        }
+
+        try (BufferedReader in = new BufferedReader(new FileReader(file1))) {
+            while (in.ready()) {
+                String readedString = in.readLine();
+                String[] splitedReadedString = readedString.split(" ");
+
+                int wordCount = 0;
+                for (String s : splitedReadedString) {
+                    if (words.contains(s)) {
+                        wordCount++;
+                    }
+                }
+
+                if (wordCount == 2)
+                    System.out.println(readedString);
+            }
+        } catch (IOException ignore) {
+            /* NOP */
+        }
     }
 }
