5d7542ec23671e8791952ff3f1983ac1bb938639
SergUstin <btboy_80@mail.ru>
2023-07-24 13:26:35 +0300

    add MVC JavaMultithreading task3608 7/9

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,14 +1,25 @@
 package com.javarush.task.task36.task3608.controller;
 
+import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
     private Model model;
     private UsersView usersView;
+    private EditUserView editUserView;
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
+    }
 
     public void setModel(Model model) {
         this.model = model;
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 
     public void onShowAllUsers() {
@@ -16,7 +27,13 @@ public class Controller {
         usersView.refresh(model.getModelData());
     }
 
-    public void setUsersView(UsersView usersView) {
-        this.usersView = usersView;
+    public void onShowAllDeletedUsers() {
+        model.loadDeletedUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void onOpenUserEditForm(long userId) {
+        model.loadUserById(userId);
+        editUserView.refresh(model.getModelData());
     }
 }
