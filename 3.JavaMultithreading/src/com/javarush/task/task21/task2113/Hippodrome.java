ca4e93c54e9dc00eddb343c90cd6c0b05b53734c
SergUstin <btboy_80@mail.ru>
2023-07-25 13:58:26 +0300

    add JavaMultithreading task2112 Hippodrome 3,4

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    private List<Horse> horses = new ArrayList<>();
+    private List<Horse> horses;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
 
     public List<Horse> getHorses() {
         return horses;
