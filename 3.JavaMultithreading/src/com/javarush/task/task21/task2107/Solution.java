325936e7e3e96ee9f7594a27b4a089ce3ed8d5e7
SergUstin <btboy_80@mail.ru>
2023-07-25 13:49:22 +0300

    add JavaMultithreading task2107

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -0,0 +1,77 @@
+package com.javarush.task.task21.task2107;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/* 
+Глубокое клонирование карты
+*/
+
+public class Solution implements Cloneable {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone = null;
+        try {
+            clone = (Solution) solution.clone();
+            System.out.println(solution);
+            System.out.println(clone);
+
+            System.out.println(solution.users);
+            System.out.println(clone.users);
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    protected Map<String, User> users = new LinkedHashMap<>();
+
+    public static class User implements Cloneable {
+        int age;
+        String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            User user = (User) o;
+
+            if (age != user.age) return false;
+            return name != null ? name.equals(user.name) : user.name == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = age;
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            return result;
+        }
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        Solution o = (Solution) super.clone();
+
+        Map<String, User> newUsers = new LinkedHashMap<>();
+        for (String string : o.users.keySet()) {
+            User user = o.users.get(string);
+            newUsers.put(string, (User) user.clone());
+        }
+        o.users = newUsers;
+
+        return o;
+    }
+}
