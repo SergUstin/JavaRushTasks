8031b383e21b29d0ce22c049d96746dfd00c57f8
SergUstin <btboy_80@mail.ru>
2023-07-25 13:49:41 +0300

    add JavaMultithreading task2110

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2110/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task21.task2110;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/* 
+Рефакторинг методов
+*/
+
+public class Solution {
+    public static void writeZipEntriesToFile(String zipFileName, String outputFileName) {
+        Charset charset = StandardCharsets.UTF_8;
+        Path outputFilePath = Paths.get(outputFileName);
+
+        try (ZipFile zip = new ZipFile(zipFileName);
+             BufferedWriter writer = Files.newBufferedWriter(outputFilePath, charset)) {
+            String newLine = System.getProperty("line.separator");
+            for (Enumeration entries = zip.entries(); entries.hasMoreElements(); ) {
+                // Берем имя файла из архива и записываем его в результирующий файл
+                // Get the entry name and write it to the output file
+                String zipEntryName = ((ZipEntry) entries.nextElement()).getName() + newLine;
+                writer.write(zipEntryName, 0, zipEntryName.length());
+            }
+        } catch (IOException ignored) {
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
