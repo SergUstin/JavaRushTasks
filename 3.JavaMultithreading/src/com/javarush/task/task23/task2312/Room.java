65cd888b52c09b5829cf9241f64be202c23b57c2
SergUstin <btboy_80@mail.ru>
2023-08-03 13:52:25 +0300

    add JavaMultithreading task2312 Snake 13

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -1,12 +1,16 @@
 package com.javarush.task.task23.task2312;
 
-public class Room {
 
+import java.awt.event.KeyEvent;
+
+/**
+ * Основной класс программы.
+ */
+public class Room {
     private int width;
     private int height;
     private Snake snake;
     private Mouse mouse;
-    public static Room game;
 
     public Room(int width, int height, Snake snake) {
         this.width = width;
@@ -14,37 +18,107 @@ public class Room {
         this.snake = snake;
     }
 
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     public Snake getSnake() {
         return snake;
-    }
-
-    public void setSnake(Snake snake) {
-        this.snake = snake;
     }
 
     public Mouse getMouse() {
         return mouse;
     }
 
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
     public void setMouse(Mouse mouse) {
         this.mouse = mouse;
     }
+
+    /**
+     * Основной цикл программы.
+     * Тут происходят все важные действия
+     */
+    public void run() {
+        //Создаем объект "наблюдатель за клавиатурой" и стартуем его.
+        KeyboardObserver keyboardObserver = new KeyboardObserver();
+        keyboardObserver.start();
+
+        //пока змея жива
+        while (snake.isAlive()) {
+            //"наблюдатель" содержит события о нажатии клавиш?
+            if (keyboardObserver.hasKeyEvents()) {
+                KeyEvent event = keyboardObserver.getEventFromTop();
+                //Если равно символу 'q' - выйти из игры.
+                if (event.getKeyChar() == 'q') return;
+
+                //Если "стрелка влево" - сдвинуть фигурку влево
+                if (event.getKeyCode() == KeyEvent.VK_LEFT)
+                    snake.setDirection(SnakeDirection.LEFT);
+                    //Если "стрелка вправо" - сдвинуть фигурку вправо
+                else if (event.getKeyCode() == KeyEvent.VK_RIGHT)
+                    snake.setDirection(SnakeDirection.RIGHT);
+                    //Если "стрелка вверх" - сдвинуть фигурку вверх
+                else if (event.getKeyCode() == KeyEvent.VK_UP)
+                    snake.setDirection(SnakeDirection.UP);
+                    //Если "стрелка вниз" - сдвинуть фигурку вниз
+                else if (event.getKeyCode() == KeyEvent.VK_DOWN)
+                    snake.setDirection(SnakeDirection.DOWN);
+            }
+
+            snake.move();   //двигаем змею
+            print();        //отображаем текущее состояние игры
+            sleep();        //пауза между ходами
+        }
+
+        //Выводим сообщение "Game Over"
+        System.out.println("Game Over!");
+    }
+
+    /**
+     * Выводим на экран текущее состояние игры
+     */
+    public void print() {
+        //Создаем массив, куда будем "рисовать" текущее состояние игры
+        //Рисуем все кусочки змеи
+        //Рисуем мышь
+        //Выводим все это на экран
+    }
+
+    /**
+     * Метод вызывается, когда мышь съели
+     */
+    public void eatMouse() {
+        createMouse();
+    }
+
+    /**
+     * Создает новую мышь
+     */
+    public void createMouse() {
+        int x = (int) (Math.random() * width);
+        int y = (int) (Math.random() * height);
+
+        mouse = new Mouse(x, y);
+    }
+
+
+    public static Room game;
 
     public static void main(String[] args) {
         game = new Room(20, 20, new Snake(10, 10));
@@ -53,25 +127,19 @@ public class Room {
         game.run();
     }
 
-    void run() {
-    }
 
-    void print() {
+    private int initialDelay = 520;
+    private int delayStep = 20;
 
-    }
-
-    void sleep() {
-
-    }
-
-    void createMouse() {
-        int x = (int) (Math.random() * width);
-        int y = (int) (Math.random() * height);
-
-        mouse = new Mouse(x, y);
-    }
-
-    public void eatMouse() {
-        createMouse();
+    /**
+     * Программа делает паузу, длинна которой зависит от длины змеи.
+     */
+    public void sleep() {
+        try {
+            int level = snake.getSections().size();
+            int delay = level < 15 ? (initialDelay - delayStep * level) : 200;
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+        }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeDirection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeDirection.java
@@ -2,7 +2,7 @@ package com.javarush.task.task23.task2312;
 
 public enum SnakeDirection {
     UP,
+    RIGHT,
     DOWN,
-    LEFT,
-    RIGHT
+    LEFT
 }
