cb0ba6e7ae828aaf29686b339dbad6a39b3bb5b4
SergUstin <btboy_80@mail.ru>
2023-08-03 13:33:18 +0300

    add JavaMultithreading task2312 Snake 8

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,11 +1,26 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
     private List<SnakeSection> sections;
     private boolean isAlive;
     private SnakeDirection direction;
+
+    public Snake(int x, int y) {
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
+
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
 
     public List<SnakeSection> getSections() {
         return sections;
@@ -21,5 +36,9 @@ public class Snake {
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
+    }
+
+    public void move() {
+
     }
 }
