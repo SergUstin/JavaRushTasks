9b61ecdb8cf029249bf8b6725e17c602c8300d34
SergUstin <btboy_80@mail.ru>
2023-08-07 15:36:20 +0300

    add JavaMultithreading task2304

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2304/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task23.task2304;
+
+import java.util.List;
+import java.util.Map;
+
+/* 
+Inner 3
+*/
+
+public class Solution {
+
+    private List<Task> tasks;
+    private List<String> names;
+
+    private DbDataProvider taskDataProvider = new TaskDataProvider();
+    private DbDataProvider nameDataProvider = new NameDataProvider();
+
+    public void refresh() {
+        Map taskCriteria = MockView.getFakeTaskCriteria();
+        taskDataProvider.refreshAllData(taskCriteria);
+
+        Map nameCriteria = MockView.getFakeNameCriteria();
+        nameDataProvider.refreshAllData(nameCriteria);
+    }
+
+    private interface DbDataProvider<T> {
+        void refreshAllData(Map criteria);
+    }
+
+    class Task {
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private class TaskDataProvider implements DbDataProvider<Task> {
+
+        @Override
+        public void refreshAllData(Map criteria) {
+            tasks = MockDB.getFakeTasks(criteria);
+        }
+    }
+
+    private class NameDataProvider implements DbDataProvider<String> {
+
+        @Override
+        public void refreshAllData(Map criteria) {
+            names = MockDB.getFakeNames(criteria);
+        }
+    }
+}
