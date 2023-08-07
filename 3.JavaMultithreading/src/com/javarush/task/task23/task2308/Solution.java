f74c14b1fe067020f6cf618d69abf76f6a3a4d8a
SergUstin <btboy_80@mail.ru>
2023-08-07 16:04:35 +0300

    add JavaMultithreading task2308

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
@@ -0,0 +1,59 @@
+package com.javarush.task.task23.task2308;
+
+/* 
+Рефакторинг, вложенные классы
+*/
+
+public class Solution {
+    public class ServerNotAccessibleException extends Exception {
+        public ServerNotAccessibleException() {
+            super(Constants.SERVER_IS_CURRENTLY_NOT_ACCESSIBLE);
+        }
+
+        public ServerNotAccessibleException(Throwable cause) {
+            super(Constants.SERVER_IS_CURRENTLY_NOT_ACCESSIBLE, cause);
+        }
+    }
+
+    public class UnauthorizedUserException extends Exception {
+        public UnauthorizedUserException() {
+            super(Constants.USER_IS_NOT_AUTHORIZED);
+        }
+
+        public UnauthorizedUserException(Throwable cause) {
+            super(Constants.USER_IS_NOT_AUTHORIZED, cause);
+        }
+    }
+
+    public class BannedUserException extends Exception {
+        public BannedUserException() {
+            super(Constants.USER_IS_BANNED);
+        }
+
+        public BannedUserException(Throwable cause) {
+            super(Constants.USER_IS_BANNED, cause);
+        }
+    }
+
+    public class RestrictionException extends Exception {
+        public RestrictionException() {
+            super(Constants.ACCESS_IS_DENIED);
+        }
+
+        public RestrictionException(Throwable cause) {
+            super(Constants.ACCESS_IS_DENIED, cause);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public final class Constants {
+        public static final String SERVER_IS_CURRENTLY_NOT_ACCESSIBLE = "The server is not currently accessible.";
+        public static final String USER_IS_NOT_AUTHORIZED = "The user is not authorized.";
+        public static final String USER_IS_BANNED = "The user is banned.";
+        public static final String ACCESS_IS_DENIED = "Access is denied.";
+
+    }
+}
