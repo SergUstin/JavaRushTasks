4af002ccca39d2494f20ad342dbe3851fb73cd85
SergUstin <btboy_80@mail.ru>
2023-08-07 16:10:55 +0300

    add JavaMultithreading task2413 Arkanoid 3/20

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -4,6 +4,11 @@ public class Arkanoid {
     private int width;
     private int height;
 
+    public Arkanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public int getWidth() {
         return width;
     }
