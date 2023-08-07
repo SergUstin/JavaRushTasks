a3655e62357b0c7103a14423c57ec80caa9fc6d4
SergUstin <btboy_80@mail.ru>
2023-08-07 16:36:33 +0300

    add JavaMultithreading task2413 Arkanoid 13/20

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,8 +1,42 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject {
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+
+    //скорость
+    private double speed;
+    //направление  (в градусах от 0 до 360)
+    private double direction;
+
+    //текущее значение вектора движения (dx,dy)
+    private double dx;
+    private double dy;
+
+    //заморожен ли объект или может двигаться
+    private boolean isFrozen;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+
+        this.direction = direction;
+        this.speed = speed;
+
+        this.isFrozen = true;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
     }
 
     @Override
