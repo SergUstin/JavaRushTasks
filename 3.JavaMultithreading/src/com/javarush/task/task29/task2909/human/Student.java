009b6e65ea8615d341d3693d32225988f1fe922a
Serg <s.ustin.92@mail.ru>
2023-08-08 13:21:04 +0300

    add JavaMultithreading task2909 Refactoring 4/15

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Student extends Human {
+public class Student extends UniversityPerson {
 
     private double averageGrade;
-    private String university;
     private Date beginningOfSession;
     private Date endOfSession;
     private int course;
@@ -26,14 +25,6 @@ public class Student extends Human {
     }
 
     public void learn() {
-    }
-
-    public String getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(String university) {
-        this.university = university;
     }
 
     public void printData() {
