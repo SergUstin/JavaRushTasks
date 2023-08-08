0e7a74620df430cf7f85074fb53167054320194f
Serg <s.ustin.92@mail.ru>
2023-08-08 14:01:25 +0300

    add JavaMultithreading task2909 Refactoring 6/15

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task29.task2909.human;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class University {
@@ -38,17 +39,21 @@ public class University {
         this.age = age;
     }
 
-    public Student getStudentWithAverageGrade() {
-        //TODO:
-        return null;
+    public Student getStudentWithAverageGrade(double averageGrade) {
+        return students.stream().filter(student -> student.getAverageGrade() == averageGrade)
+                .findFirst()
+                .get();
     }
 
-    public Student getStudentWithMaxAverageGrade(double averageGrade) {
-        //TODO:
-        return null;
+    public Student getStudentWithMaxAverageGrade() {
+        return students.stream().max(Comparator.comparingDouble(Student::getAverageGrade)).get();
     }
 
-    public void getStudentWithMinAverageGradeAndExpel() {
-        //TODO:
+    public Student getStudentWithMinAverageGrade() {
+        return students.stream().min(Comparator.comparingDouble(Student::getAverageGrade)).get();
+    }
+
+    public void expel(Student student) {
+        students.remove(student);
     }
 }
