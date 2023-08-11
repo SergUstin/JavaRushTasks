e2ef5c2e33613d43eec4e60d918adb55d39b3b47
Serg <s.ustin.92@mail.ru>
2023-08-11 10:09:15 +0300

    add JavaMultithreading task2406

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2406/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task24.task2406;
+
+import java.math.BigDecimal;
+
+/* 
+Наследование от внутреннего класса
+*/
+
+public class Solution {
+    public class Building {
+        public class Hall {
+            private BigDecimal square;
+
+            public Hall(BigDecimal square) {
+                this.square = square;
+            }
+        }
+
+        public class Apartments {
+        }
+    }
+
+    public class Apt3Bedroom extends Building.Apartments {
+        public Apt3Bedroom(Building building) {
+            building.super();
+        }
+    }
+
+    public class BigHall extends Building.Hall {
+        public BigHall(Building building, BigDecimal square) {
+            building.super(square);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
