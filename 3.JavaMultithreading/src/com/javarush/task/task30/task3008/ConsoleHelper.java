a12de53c7f70243f761efb2f704a2ace92a6b5ed
Serg <s.ustin.92@mail.ru>
2023-08-15 13:05:59 +0300

    add JavaMultithreading task3008 Chat 6/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -7,6 +7,9 @@ import java.io.InputStreamReader;
 public class ConsoleHelper {
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+    private ConsoleHelper() {
+    }
+
     public static void writeMessage(String message) {
         System.out.println(message);
     }
