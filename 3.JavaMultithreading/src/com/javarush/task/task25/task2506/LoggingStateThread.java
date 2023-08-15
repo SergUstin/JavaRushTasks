8a2d64d3b75b2366d876bb002f858a13df0adf22
Serg <s.ustin.92@mail.ru>
2023-08-15 17:10:05 +0300

    add JavaMultithreading task2506

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2506/LoggingStateThread.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task25.task2506;
+
+public class LoggingStateThread extends Thread {
+
+    private Thread thread;
+
+    public LoggingStateThread(Thread thread) {
+        this.thread = thread;
+        setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+        State currentState = thread.getState();
+        System.out.println(currentState);
+
+        State newState;
+        do {
+            if ((newState = thread.getState()) != currentState) {
+                currentState = newState;
+                System.out.println(newState);
+            }
+        } while (!currentState.equals(State.TERMINATED));
+    }
+}
