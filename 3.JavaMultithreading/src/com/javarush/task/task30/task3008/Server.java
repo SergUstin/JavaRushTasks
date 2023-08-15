ad4af0cf129958a8a54b35248fcddab7058ac791
Serg <s.ustin.92@mail.ru>
2023-08-15 13:17:12 +0300

    add JavaMultithreading task3008 Chat 7/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,24 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Не смогли отправить сообщение! " +
+                        connection.getRemoteSocketAddress());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера!");
         int port = ConsoleHelper.readInt();
