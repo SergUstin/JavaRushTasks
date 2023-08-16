cff141380d395fb9ba12f031d8fcd5b0e5b81d79
Serg <s.ustin.92@mail.ru>
2023-08-16 13:58:35 +0300

    add JavaMultithreading task3008 Chat 14/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -44,7 +44,50 @@ public class Client {
         }
     }
 
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+
+        try {
+            synchronized (this) {
+                this.wait();
+            }
+        } catch (InterruptedException e) {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента");
+            return;
+        }
+
+        if (clientConnected) {
+            ConsoleHelper.writeMessage("Соединение установлено. " +
+                    "Для выхода наберите команду 'exit'.");
+            while (clientConnected) {
+                String text = ConsoleHelper.readString();
+                if (text.equals("exit")) {
+                    break;
+                }
+                if (shouldSendTextFromConsole()) {
+                    sendTextMessage(text);
+                }
+            }
+        } else {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
     public class SocketThread extends Thread {
+
+        @Override
+        public void run() {
+
+
+        }
 
     }
 }
