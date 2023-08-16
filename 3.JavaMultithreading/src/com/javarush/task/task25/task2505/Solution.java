565c2ed3867c95973f569dfc514dc4adf0728b98
Serg <s.ustin.92@mail.ru>
2023-08-16 16:13:21 +0300

    add JavaMultithreading task2505

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2505/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task25.task2505;
+
+/* 
+Без дураков
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        MyThread myThread = new Solution().new MyThread("super secret key");
+        myThread.start();
+    }
+
+    public class MyThread extends Thread {
+        private String secretKey;
+
+        public MyThread(String secretKey) {
+            this.secretKey = secretKey;
+            setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+//            setDaemon(true);
+        }
+
+        @Override
+        public void run() {
+            throw new NullPointerException("it's an example");
+        }
+
+        private class MyUncaughtExceptionHandler implements UncaughtExceptionHandler {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e1) {
+                }
+                System.out.println(String.format("%s, %s, %s", MyThread.this.secretKey, t.getName(), e.getMessage()));
+
+            }
+        }
+    }
+
+}
+
