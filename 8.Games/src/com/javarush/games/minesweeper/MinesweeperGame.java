b277e895ed7cabdc4824f83d9ae4c453700acb8d
Serg <s.ustin.92@mail.ru>
2023-08-18 11:08:34 +0300

    add MinesweeperGame 2/16

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,4 +3,11 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
+
+    private static final int SIDE = 9;
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
 }
