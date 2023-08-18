4e1b0154efa7b4ef7dc88b63c77f848157dce8c6
Serg <s.ustin.92@mail.ru>
2023-08-18 11:17:22 +0300

    add MinesweeperGame 4/16

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -1,11 +1,10 @@
 package com.javarush.games.minesweeper;
 
 public class GameObject {
-
     public int x;
     public int y;
 
-    public GameObject(int x, int y) {
+    GameObject(int x, int y) {
         this.x = x;
         this.y = y;
     }
--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -5,9 +5,20 @@ import com.javarush.engine.cell.*;
 public class MinesweeperGame extends Game {
 
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                gameField[y][x] = new GameObject(x, y);
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
     }
 }
