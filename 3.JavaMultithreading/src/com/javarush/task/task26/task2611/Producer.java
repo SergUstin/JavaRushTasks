24be77898c71c99be83aa8d06057ab83d338c838
Serg <s.ustin.92@mail.ru>
2023-08-21 16:07:03 +0300

    add JavaMultithreading task2611

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2611/Producer.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task26.task2611;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Producer implements Runnable {
+
+    private ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap<String, String> map) {
+        this.map = map;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int i = 1;
+            while (true) {
+                map.put(String.valueOf(i), "Some text for " + i);
+                i++;
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+
+    }
+}
