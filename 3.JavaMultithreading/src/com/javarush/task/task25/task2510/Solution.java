e964501d9be154b50122a8b21f2a72a7698c4374
Serg <s.ustin.92@mail.ru>
2023-08-21 11:46:25 +0300

    add JavaMultithreading task2510

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task25.task2510;
+
+/* 
+Поживем - увидим
+*/
+
+public class Solution extends Thread {
+
+    public Solution() {
+        this.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                if (e instanceof Error) {
+                    System.out.println("Нельзя дальше работать");
+                } else if (e instanceof Exception) {
+                    System.out.println("Надо обработать");
+                } else {
+                    System.out.println("Поживем - увидим");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+    }
+}
