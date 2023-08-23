bcc52bcc50e0cac8abe1f855a38febed85be39f4
Serg <s.ustin.92@mail.ru>
2023-08-23 15:48:02 +0300

    add JavaMultithreading task3209 HTML Editor 9/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -16,6 +16,14 @@ public class View extends JFrame implements ActionListener {
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
 
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
     public Controller getController() {
         return controller;
     }
@@ -40,7 +48,18 @@ public class View extends JFrame implements ActionListener {
         controller.exit();
     }
 
-    public void initMenuBar() {}
+    public void initMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this, menuBar);
+        MenuHelper.initEditMenu(this, menuBar);
+        MenuHelper.initStyleMenu(this, menuBar);
+        MenuHelper.initAlignMenu(this, menuBar);
+        MenuHelper.initColorMenu(this, menuBar);
+        MenuHelper.initFontMenu(this, menuBar);
+        MenuHelper.initHelpMenu(this, menuBar);
+
+        getContentPane().add(menuBar, BorderLayout.NORTH);
+    }
 
     public void initEditor() {
         htmlTextPane.setContentType("text/html");
--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/StrikeThroughAction.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/StrikeThroughAction.java
@@ -4,7 +4,7 @@ import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
 
 public class StrikeThroughAction extends StyledEditorKit.StyledTextAction {
-
+    
     public StrikeThroughAction() {
         super(null);
     }
