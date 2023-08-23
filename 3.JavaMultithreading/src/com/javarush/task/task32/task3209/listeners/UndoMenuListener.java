1de987c5e7eab9fca2e9a670f757d908f5c7e7c3
Serg <s.ustin.92@mail.ru>
2023-08-23 15:36:42 +0300

    add JavaMultithreading task3209 HTML Editor 8/24

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/UndoMenuListener.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task32.task3209.listeners;
+
+import com.javarush.task.task32.task3209.View;
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class UndoMenuListener implements MenuListener {
+
+    private View view;
+
+    public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
+        this.view = view;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+}
