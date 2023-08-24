b7e1bd62ce147f925f35c5e792bb6b8d1bde24be
Serg <s.ustin.92@mail.ru>
2023-08-24 11:14:44 +0300

    add JavaMultithreading task3209 HTML Editor 12/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SubscriptAction.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SubscriptAction.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task32.task3209.actions;
 
+import javax.swing.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
@@ -11,7 +14,13 @@ public class SubscriptAction extends StyledEditorKit.StyledTextAction {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void actionPerformed(ActionEvent actionEvent) {
+        JEditorPane editor = getEditor(actionEvent);
+        if (editor != null) {
+            MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+            SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+            StyleConstants.setSubscript(simpleAttributeSet, !StyleConstants.isSubscript(mutableAttributeSet));
+            setCharacterAttributes(editor, simpleAttributeSet, false);
+        }
     }
 }
