655ccde40af5996cab172ccc0769389522a27c36
Serg <s.ustin.92@mail.ru>
2023-08-29 22:13:41 +0300

    add JavaMultithreading task2710

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task27.task2710;
+
+public class MailServer implements Runnable {
+    private Mail mail;
+
+    public MailServer(Mail mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        try {
+            synchronized (mail) {
+                mail.wait();
+            }
+            String name = Thread.currentThread().getName();
+            long endTime = System.currentTimeMillis();
+            System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
