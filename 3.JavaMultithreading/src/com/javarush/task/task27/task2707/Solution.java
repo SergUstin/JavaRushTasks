e47b6d11a73e162dfc58c96055aaaa2d30697ffd
Serg <s.ustin.92@mail.ru>
2023-08-29 22:09:00 +0300

    add JavaMultithreading task2707

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -13,9 +13,41 @@ public class Solution {
         }
     }
 
+    private static volatile boolean flag = false;
+    private static volatile boolean isInnerThreadBlocked = false;
+
     public static boolean isLockOrderNormal(final Solution solution, final Object o1, final Object o2) throws Exception {
         //do something here
-        return false;
+        synchronized (o1) {
+            Thread outerThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Thread innerThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            solution.someMethodWithSynchronizedBlocks(o1, o2);
+                        }
+                    });
+                    innerThread.start();
+
+                    while (innerThread.getState() != Thread.State.BLOCKED) ;
+                    isInnerThreadBlocked = true;
+                    synchronized (o2) {
+                        flag = true;
+                    }
+                }
+            });
+            outerThread.setDaemon(true);
+            outerThread.start();
+
+            while (!isInnerThreadBlocked) {
+                Thread.sleep(1);
+            }
+            while (outerThread.getState() != Thread.State.BLOCKED && outerThread.isAlive()) {
+                Thread.sleep(1);
+            }
+        }
+        return flag;
     }
 
     public static void main(String[] args) throws Exception {
