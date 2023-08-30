1b843f14bcad3403509de6869a31abf29295dd3f
Serg <s.ustin.92@mail.ru>
2023-08-30 13:39:50 +0300

    add JavaMultithreading task3110 Archiver 5/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ExitCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ExitCommand.java
@@ -1,9 +1,8 @@
 package com.javarush.task.task31.task3110.command;
 
-
 import com.javarush.task.task31.task3110.ConsoleHelper;
 
-public class ExitCommand implements Command {
+public class ExitCommand extends ZipCommand implements Command {
     @Override
     public void execute() throws Exception {
         ConsoleHelper.writeMessage("До встречи!");
