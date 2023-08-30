373d68988fa11909a27de943166f00fdca344511
Serg <s.ustin.92@mail.ru>
2023-08-30 16:57:54 +0300

    add MinesweeperGame 7/16

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -4,6 +4,7 @@ public class GameObject {
     public int x;
     public int y;
     public boolean isMine;
+    public boolean isOpen;
     public int countMineNeighbors;
 
     GameObject(int x, int y, boolean isMine) {
--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -1,7 +1,6 @@
 package com.javarush.games.minesweeper;
 
-import com.javarush.engine.cell.Color;
-import com.javarush.engine.cell.Game;
+import com.javarush.engine.cell.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,10 +10,18 @@ public class MinesweeperGame extends Game {
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
     private int countMinesOnField;
 
+    private static final String MINE = "\uD83D\uDCA3";
+
+
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
         createGame();
+    }
+
+    @Override
+    public void onMouseLeftClick(int x, int y) {
+        openTile(x, y);
     }
 
     private void createGame() {
@@ -64,5 +71,17 @@ public class MinesweeperGame extends Game {
             }
         }
         
+    }
+
+    private void openTile(int x, int y) {
+        GameObject gameObject = gameField[y][x];
+
+        if (gameObject.isMine) {
+            setCellValue(gameObject.x, gameObject.y, MINE);
+        } else {
+            setCellNumber(x, y, gameObject.countMineNeighbors);
+        }
+        gameObject.isOpen = true;
+        setCellColor(x, y, Color.GREEN);
     }
 }
