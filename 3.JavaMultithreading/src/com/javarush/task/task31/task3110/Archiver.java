d6eb1507a726bfe7248ed595305de7d63ecf8caf
Serg <s.ustin.92@mail.ru>
2023-08-30 13:33:10 +0300

    add JavaMultithreading task3110 Archiver 4/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
@@ -14,6 +16,9 @@ public class Archiver {
         System.out.println("Введите путь для архива: ");
         String src = console.readLine();
         zipFileManager.createZip(Paths.get(src));
+
+        ExitCommand exitCommand = new ExitCommand();
+        exitCommand.execute();
 
     }
 }
