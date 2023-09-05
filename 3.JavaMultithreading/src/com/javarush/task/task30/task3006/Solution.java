341c84dc848939e4461fa9753f91cdbd05697575
Serg <s.ustin.92@mail.ru>
2023-09-05 19:09:32 +0300

    add JavaMultithreading task3006

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3006/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task30.task3006;
+
+/* 
+Swap по-новому
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Pair pair = new Pair(4, 5);
+        System.out.println(pair);
+        pair.swap();
+        System.out.println(pair);
+    }
+}
