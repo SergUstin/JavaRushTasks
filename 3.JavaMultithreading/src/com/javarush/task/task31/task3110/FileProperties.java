4fff5e52685d11c98040a902a19835eb890bc68d
Serg <s.ustin.92@mail.ru>
2023-09-05 13:40:21 +0300

    add JavaMultithreading task3110 Archiver 12/18

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task31.task3110;
+
+public class FileProperties {
+    private String name;
+    private long size;
+    private long compressedSize;
+    private int compressionMethod;
+
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public long getCompressedSize() {
+        return compressedSize;
+    }
+
+    public int getCompressionMethod() {
+        return compressionMethod;
+    }
+
+    public long getCompressionRatio() {
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        return size > 0 ? String.format("%s %d Kb (%s Kb) сжатие: %d%%", name, size / 1024, compressedSize / 1024, getCompressionRatio()) : name;
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -9,7 +9,8 @@ import java.nio.file.Paths;
 public abstract class ZipCommand implements Command {
 
     public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь файла архива!");
-        return new ZipFileManager(Paths.get(ConsoleHelper.readString()));
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
