1cbf31361c38754885ea070c3bf1f05160b02381
Serg <s.ustin.92@mail.ru>
2023-09-07 11:47:51 +0300

    add JavaMultithreading task3003

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Consumer.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task30.task3003;
+
+import java.util.concurrent.TransferQueue;
+
+public class Consumer implements Runnable {
+    public Consumer(TransferQueue<ShareItem> queue) {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Producer.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task30.task3003;
+
+import java.util.concurrent.TransferQueue;
+
+public class Producer implements Runnable {
+    public Producer(TransferQueue<ShareItem> queue) {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
