7b74c566347c6ea4c869cc54a73069a3d17775a7
Serg <s.ustin.92@mail.ru>
2023-09-07 15:07:05 +0300

    add JavaMultithreading task3003

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Consumer.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Consumer.java
@@ -3,11 +3,22 @@ package com.javarush.task.task30.task3003;
 import java.util.concurrent.TransferQueue;
 
 public class Consumer implements Runnable {
+    private TransferQueue<ShareItem> queue;
+
     public Consumer(TransferQueue<ShareItem> queue) {
+        this.queue = queue;
     }
 
     @Override
     public void run() {
+        try {
+            Thread.sleep(450);
+            while (true) {
+                ShareItem take = queue.take();
+                System.out.format("Processing %s\n", take);
+            }
+        } catch (InterruptedException e) {
+        }
 
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Producer.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Producer.java
@@ -3,11 +3,28 @@ package com.javarush.task.task30.task3003;
 import java.util.concurrent.TransferQueue;
 
 public class Producer implements Runnable {
+    private TransferQueue<ShareItem> queue;
+
     public Producer(TransferQueue<ShareItem> queue) {
+        this.queue = queue;
     }
 
     @Override
     public void run() {
+        try {
+            for (int i = 1; i < 10; i++) {
+                System.out.format("Элемент 'ShareItem-%d' добавлен\n", i);
+                queue.offer(new ShareItem("ShareItem-" + i, i));
+                Thread.sleep(100);
+                if (queue.hasWaitingConsumer()) {
+                    System.out.format("Consumer в ожидании!\n");
+                }
+
+            }
+
+        } catch (InterruptedException e) {
+
+        }
 
     }
 }
