3e2cdd24915cb374c7270b2f11898786b8534200
Serg <s.ustin.92@mail.ru>
2023-09-11 14:37:14 +0300

    add JavaMultithreading task3102

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task31.task3102;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+/* 
+Находим все файлы
+*/
+
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        List<String> result = new ArrayList<>();
+
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get(root), options, 20, new GetFiles(result));
+
+        return result;
+    }
+
+    private static class GetFiles extends SimpleFileVisitor<Path> {
+        private List<String> result;
+
+        public GetFiles(List<String> result) {
+            this.result = result;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            String s = file.toAbsolutePath().toString();
+            result.add(s);
+            return FileVisitResult.CONTINUE;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
