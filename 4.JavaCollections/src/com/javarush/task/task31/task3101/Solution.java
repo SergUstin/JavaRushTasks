1f76e30660589bf2a988134290612cb3b9be1cc4
Serg <s.ustin.92@mail.ru>
2023-09-11 14:32:11 +0300

    add JavaMultithreading task3101

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -15,6 +15,54 @@ import java.util.TreeMap;
 
 public class Solution {
     public static void main(String[] args) {
+        String path = args[0];
+        String resultFileAbsolutePath = args[1];
+        try {
+            File resultFile = new File(resultFileAbsolutePath);
+            File dest = new File(resultFile.getParentFile() + "/allFilesContent.txt");
+            if (FileUtils.isExist(dest)) {
+                FileUtils.deleteFile(dest);
+            }
+            FileUtils.renameFile(resultFile, dest);
 
+            Map<String, byte[]> fileTree = getFileTree(path);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(dest)) {
+                for (byte[] bytes : fileTree.values()) {
+                    fileOutputStream.write(bytes);
+                    fileOutputStream.write("\n".getBytes());
+                }
+            }
+        } catch (IOException ignored){
+
+        }
     }
+
+    public static Map<String, byte[]> getFileTree(String root) throws IOException {
+        Map<String, byte[]> result = new TreeMap<>();
+
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get(root), options, 20, new GetFiles(result));
+
+        return result;
+    }
+
+    private static class GetFiles extends SimpleFileVisitor<Path> {
+        private Map<String, byte[]> result;
+
+        public GetFiles(Map<String, byte[]> result) {
+            this.result = result;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+            File file = path.toFile();
+            if (file.isFile()) {
+                if (file.length() <= 50) {
+                    result.put(path.getFileName().toString(), Files.readAllBytes(path));
+                }
+            }
+            return super.visitFile(path, attrs);
+        }
+    }
+
 }
