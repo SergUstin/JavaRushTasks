968fcfce6739a52d39299752f2e0e27219560236
Serg <s.ustin.92@mail.ru>
2023-09-13 13:30:01 +0300

    add Annotation task3803

--- 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/Solution.java
@@ -4,20 +4,35 @@ package com.javarush.task.jdk13.task38.task3803;
 Обработка аннотаций
 */
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         printFullyQualifiedNames(Solution.class);
         printFullyQualifiedNames(SomeTest.class);
+        System.out.println("------");
 
         printValues(Solution.class);
         printValues(SomeTest.class);
     }
 
     public static <T> boolean printFullyQualifiedNames(Class<T> c) {
-        return true;
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            PrepareMyTest annotation = c.getAnnotation(PrepareMyTest.class);
+            Arrays.stream(annotation.fullyQualifiedNames()).forEach(System.out::println);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static <T> boolean printValues(Class<T> c) {
-        return true;
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            PrepareMyTest annotation = c.getAnnotation(PrepareMyTest.class);
+            Arrays.stream(annotation.value()).map(Class::getSimpleName).forEach(System.out::println);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/SomeTest.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/SomeTest.java
@@ -1,5 +1,6 @@
 package com.javarush.task.jdk13.task38.task3803;
 
-@PrepareMyTest(value = {Solution.class, SomeTest.class}, fullyQualifiedNames = {"com.javarush.task.jdk13.task38.task3803.Fox"})
+@PrepareMyTest(value = {Solution.class, SomeTest.class},
+        fullyQualifiedNames = {"com.javarush.task.jdk13.task38.task3803.Fox"})
 public class SomeTest {
 }
