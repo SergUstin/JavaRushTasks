2aec22e0f501d71f20394cab1a2bc5ef3c258d99
Serg <s.ustin.92@mail.ru>
2023-09-13 14:02:06 +0300

    add Annotation task3405

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3405/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.jdk13.task34.task3405;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Нарушение приватности
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        User user = new User("Amigo", 21);
+        print(getFields(user));
+
+        Task task = new Task("task3405", true);
+        print(getFields(task));
+    }
+
+    public static Map<String, Object> getFields(Object object) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        Field[] fields = object.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (Modifier.isPrivate(field.getModifiers())) {
+                field.setAccessible(true);
+                String key = field.getName();
+                Object value = field.get(object);
+                map.put(key, value);
+            }
+        }
+        return map;
+    }
+
+    public static void print(Map<?, ?> fields) {
+        System.out.println("-------------------");
+        fields.forEach((fieldName, fieldValue) -> System.out.println(fieldName + " = " + fieldValue));
+    }
+}
