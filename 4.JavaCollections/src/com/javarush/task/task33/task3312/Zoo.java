00ab9b7268cdd8126dc33c09e30f530975b5493c
Serg <s.ustin.92@mail.ru>
2023-09-14 19:49:25 +0300

    add JavaCollection task3312

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3312/Zoo.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task33.task3312;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+    public List<Animal> animals = new ArrayList<>();
+
+    @JsonTypeInfo (
+            use = JsonTypeInfo.Id.NAME,
+            include = JsonTypeInfo.As.PROPERTY,
+            property = "type"
+    )
+    @JsonSubTypes ({
+            @JsonSubTypes.Type(value = Dog.class, name = "dog"),
+            @JsonSubTypes.Type(value = Cat.class, name = "cat")
+    })
+    public static class Animal {
+        public Animal(String name) {
+            this.name = name;
+        }
+
+        public String name;
+    }
+
+    @JsonTypeName("dog")
+    public static class Dog extends Animal {
+
+        public double barkVolume;
+
+        public Dog(String name) {
+            super(name);
+        }
+    }
+
+    @JsonTypeName("cat")
+    public static class Cat extends Animal {
+        boolean likesCream;
+        public int lives;
+
+        public Cat(String name) {
+            super(name);
+        }
+    }
+}
