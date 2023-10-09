7776f9711009eb70cf440e5a011887ff6b3ccecd
Serg <s.ustin.92@mail.ru>
2023-10-09 12:31:47 +0300

    add JavaCollection task3402

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task34.task3402;
+
+/* 
+Факториал с помощью рекурсии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.factorial(9));     //362880
+        System.out.println(solution.factorial(0));     //1
+        System.out.println(solution.factorial(1));     //1
+    }
+
+    public int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
