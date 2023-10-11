54230e5d26eec2f1d01d667209d3af7ebf49ae32
Serg <s.ustin.92@mail.ru>
2023-10-11 17:35:47 +0300

    add MoonLanderGame 6/23

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -6,15 +6,17 @@ public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private Rocket rocket;
+    private GameObject landscape;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
+        showGrid(false);
     }
 
     private void createGame() {
-        rocket = new Rocket(WIDTH / 2.0, 0);
+        createGameObjects();
         drawScene();
     }
 
@@ -26,5 +28,12 @@ public class MoonLanderGame extends Game {
         }
 
         rocket.draw(this);
+        landscape.draw(this);
+    }
+
+    private void createGameObjects() {
+        rocket = new Rocket(WIDTH / 2.0, 0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
+
     }
 }
