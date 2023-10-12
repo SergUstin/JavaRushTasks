88f64f2941df54f01280156b05d7b1226c8d14fd
Serg <s.ustin.92@mail.ru>
2023-10-12 15:23:52 +0300

    add JavaCollection task3613

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3613/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task36.task3613;
+
+/* 
+Найти класс по описанию Ӏ Java Collections: 6 уровень, 10 лекция
+*/
+
+import java.util.concurrent.SynchronousQueue;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return SynchronousQueue.class;
+    }
+}
