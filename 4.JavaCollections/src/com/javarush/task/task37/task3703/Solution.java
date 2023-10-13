ab114684f42a39446cb0a7e2d491955207977ae3
Serg <s.ustin.92@mail.ru>
2023-10-13 11:52:45 +0300

    add JavaCollection task3703

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3703/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task37.task3703;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
+/* 
+Найти класс по описанию Ӏ Java Collections: 7 уровень, 6 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return ConcurrentSkipListMap.class;
+    }
+}
