0b1ef5ecc4ae7fafe64d53191d8d23b7169e69eb
Serg <s.ustin.92@mail.ru>
2023-10-31 12:07:06 +0300

    add JavaCollection task4009

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4009/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task40.task4009;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+/* 
+Buon Compleanno!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getWeekdayOfBirthday("30.05.1984", "2015"));
+    }
+
+    public static String getWeekdayOfBirthday(String birthday, String year) {
+        DateTimeFormatter birthdayDateFormat = DateTimeFormatter.ofPattern("d.M.y");
+        LocalDate birthdayDate = LocalDate.parse(birthday, birthdayDateFormat);
+
+        DateTimeFormatter yearFormat = DateTimeFormatter.ofPattern("y");
+        Year yearDate = Year.parse(year, yearFormat);
+
+        return birthdayDate.withYear(yearDate.getValue()).format(DateTimeFormatter.ofPattern("EEEE").withLocale(Locale.ITALIAN));
+    }
+}
