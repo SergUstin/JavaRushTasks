3f652c9c0851fdbac0358cba0e576ce73ff6727f
Serg <s.ustin.92@mail.ru>
2023-11-14 16:25:57 +0300

    add Tetris 6/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -1,4 +1,25 @@
 package com.javarush.task.task22.task2213;
 
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Field(int width, int height) {
+        this.height = height;
+        this.width = width;
+        matrix = new int[height][width];
+    }
 }
