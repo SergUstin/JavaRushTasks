aa7d2b71091f15f4f836de54638b15910fc0c41a
Serg <s.ustin.92@mail.ru>
2023-11-14 15:11:13 +0300

    add RacerGame 3/28

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -24,5 +24,16 @@ public class RacerGame extends Game {
     }
 
     private void drawField() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (x == CENTER_X) {
+                    setCellColor(x, y, Color.WHITE);
+                } else if (x >= ROADSIDE_WIDTH && x < WIDTH - ROADSIDE_WIDTH) {
+                    setCellColor(x, y, Color.DIMGREY);
+                } else {
+                    setCellColor(x, y, Color.GREEN);
+                }
+            }
+        }
     }
 }
