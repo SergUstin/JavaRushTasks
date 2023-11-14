a6702d90753a5edadea22fd3a3fb2ca9525e46cb
Serg <s.ustin.92@mail.ru>
2023-11-14 16:17:09 +0300

    add Game2048_MVC 9/17

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,10 +1,17 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Controller extends KeyAdapter {
     private Model model;
     private View view;
+    private static final int WINNING_TILE = 2048;
+
+    public Controller(Model model) {
+        view = new View(this);
+        this.model = model;
+    }
 
     public Tile[][] getGameTiles() {
         return model.getGameTiles();
@@ -12,5 +19,45 @@ public class Controller extends KeyAdapter {
 
     public int getScore() {
         return model.score;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            resetGame();
+        }
+        if (!model.canMove()) {
+            view.isGameLost = true;
+        }
+
+        if (!view.isGameWon && !view.isGameLost) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    model.left();
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    model.right();
+                    break;
+                case KeyEvent.VK_DOWN:
+                    model.down();
+                    break;
+                case KeyEvent.VK_UP:
+                    model.up();
+                    break;
+            }
+        }
+
+        if (model.maxTile == WINNING_TILE) {
+            view.isGameWon = true;
+        }
+
+        view.repaint();
+    }
+
+    public void resetGame() {
+        model.score = 0;
+        view.isGameWon = false;
+        view.isGameLost = false;
+        model.resetGameTiles();
     }
 }
