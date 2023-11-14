852928119d2568b93c92a0ffc9c1719ddbfc94ac
Serg <s.ustin.92@mail.ru>
2023-11-14 15:53:41 +0300

    add SpaceInvadersGame 18/34

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
@@ -1,7 +1,13 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Ship extends GameObject {
     public boolean isAlive = true;
+    private List<int[][]> frames;
+    private int frameIndex;
 
     public Ship(double x, double y) {
         super(x, y);
@@ -13,9 +19,19 @@ public class Ship extends GameObject {
 
     public void setStaticView(int[][] viewFrame) {
         setMatrix(viewFrame);
+        frames = new ArrayList<>();
+        frames.add(viewFrame);
+        frameIndex = 0;
     }
 
     public void kill() {
         isAlive = false;
+    }
+
+    public void setAnimatedView(int[][]... viewFrames) {
+        setMatrix(viewFrames[0]);
+
+        frames = Arrays.asList(viewFrames);
+        frameIndex = 0;
     }
 }
