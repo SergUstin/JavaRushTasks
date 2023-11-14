446f7191a7a6ccf623e69ed1e10baba957975fc8
Serg <s.ustin.92@mail.ru>
2023-11-14 16:24:43 +0300

    add Tetris 3/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -4,6 +4,14 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     public static void main(String[] args) {
 
     }
