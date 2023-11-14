9539e5c23fc251db4cd9b53bf9699d0b62aee2df
Serg <s.ustin.92@mail.ru>
2023-11-14 15:33:40 +0300

    add RacerGame 23/28

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -8,10 +8,12 @@ public class RacerGame extends Game {
     public static final int HEIGHT = 64;
     public static final int CENTER_X = WIDTH / 2;
     public static final int ROADSIDE_WIDTH = 14;
+    private static final int RACE_GOAL_CARS_COUNT = 40;
     private PlayerCar player;
     private RoadMarking roadMarking;
     private RoadManager roadManager;
     private boolean isGameStopped;
+    private FinishLine finishLine;
 
     @Override
     public void initialize() {
@@ -36,6 +38,7 @@ public class RacerGame extends Game {
         roadMarking = new RoadMarking();
         player = new PlayerCar();
         roadManager = new RoadManager();
+        finishLine = new FinishLine();
         drawScene();
         setTurnTimer(40);
         isGameStopped = false;
@@ -46,6 +49,7 @@ public class RacerGame extends Game {
         roadMarking.draw(this);
         roadManager.draw(this);
         player.draw(this);
+        finishLine.draw(this);
     }
 
     private void drawField() {
@@ -66,6 +70,7 @@ public class RacerGame extends Game {
         roadMarking.move(player.speed);
         roadManager.move(player.speed);
         player.move();
+        finishLine.move(player.speed);
     }
 
     private void gameOver() {
