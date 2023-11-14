4c31dbae7e2723258f2ba992c4189d4cbe75a79a
Serg <s.ustin.92@mail.ru>
2023-11-14 16:28:37 +0300

    add Tetris 13/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -25,6 +25,15 @@ public class Field {
 
     void print() {
 
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] == 0)
+                    System.out.print(".");
+                else
+                    System.out.print("X");
+            }
+            System.out.println();
+        }
     }
 
     void removeFullLines() {
