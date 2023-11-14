aed0366e179e6a6b5f6d4d763d50b3ca292cca42
Serg <s.ustin.92@mail.ru>
2023-11-14 15:13:42 +0300

    add RacerGame 5/28

---
+++ 8.Games/src/com/javarush/games/racer/GameObject.java
@@ -0,0 +1,27 @@
+package com.javarush.games.racer;
+
+import com.javarush.engine.cell.*;
+
+public class GameObject {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public int[][] matrix;
+
+    public GameObject(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+        this.width = matrix[0].length;
+        this.height = matrix.length;
+    }
+
+    public void draw(Game game) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                game.setCellColor(x + j, y + i, Color.values()[matrix[i][j]]);
+            }
+        }
+    }
+}
