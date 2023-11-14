379791a150e40cc178a95ea183d38e4b93e051bb
Serg <s.ustin.92@mail.ru>
2023-11-14 16:16:38 +0300

    add Game2048_MVC 8/17

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -14,6 +14,10 @@ public class Model {
         resetGameTiles();
     }
 
+    Tile[][] getGameTiles() {
+        return gameTiles;
+    }
+
     void resetGameTiles() {
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
         for (int i = 0; i < FIELD_WIDTH; i++) {
@@ -136,5 +140,30 @@ public class Model {
         gameTiles = rotateClockwise(gameTiles);
         gameTiles = rotateClockwise(gameTiles);
         gameTiles = rotateClockwise(gameTiles);
+    }
+
+    private int getEmptyTilesCount() {
+        return getEmptyTiles().size();
+    }
+
+    private boolean isFull() {
+        return getEmptyTilesCount() == 0;
+    }
+
+    boolean canMove() {
+        if (!isFull()) {
+            return true;
+        }
+
+        for (int x = 0; x < FIELD_WIDTH; x++) {
+            for (int y = 0; y < FIELD_WIDTH; y++) {
+                Tile t = gameTiles[x][y];
+                if ((x < FIELD_WIDTH - 1 && t.value == gameTiles[x + 1][y].value)
+                        || ((y < FIELD_WIDTH - 1) && t.value == gameTiles[x][y + 1].value)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/View.java
@@ -1,4 +1,5 @@
 package com.javarush.task.task35.task3513;
 
 public class View {
+
 }
