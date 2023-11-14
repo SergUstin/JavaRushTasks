1dd90c79bb1bfa188126b98fb1d368afb8ed09c5
Serg <s.ustin.92@mail.ru>
2023-11-14 15:49:06 +0300

    add SpaceInvadersGame 12/34

---
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Bullet.java
@@ -0,0 +1,19 @@
+package com.javarush.games.spaceinvaders.gameobjects;
+
+import com.javarush.games.spaceinvaders.Direction;
+import com.javarush.games.spaceinvaders.ShapeMatrix;
+
+public class Bullet extends GameObject {
+    private int dy;
+    public boolean isAlive = true;
+
+    public Bullet(double x, double y, Direction direction) {
+        super(x, y);
+        setMatrix(ShapeMatrix.BULLET);
+        this.dy = direction == Direction.UP ? -1 : 1;
+    }
+
+    public void move() {
+        y += dy;
+    }
+}
