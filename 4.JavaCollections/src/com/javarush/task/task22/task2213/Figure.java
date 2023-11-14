208fe3632d5a1ef8ec7cd7b7d1cce248a984e620
Serg <s.ustin.92@mail.ru>
2023-11-14 16:26:30 +0300

    add Tetris 8/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -1,4 +1,25 @@
 package com.javarush.task.task22.task2213;
 
 public class Figure {
+    private int x;
+    private int y;
+    private int[][] matrix;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
 }
