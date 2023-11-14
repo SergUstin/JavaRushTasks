62083b4ca2e2ddf5b569d2c166392857637ed356
Serg <s.ustin.92@mail.ru>
2023-11-14 16:19:49 +0300

    add Game2048_MVC 14/17

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/MoveEfficiency.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task35.task3513;
+
+class MoveEfficiency {
+    private int numberOfEmptyTiles;
+    private int score;
+    private Move move;
+
+    public MoveEfficiency(int numberOfEmptyTiles, int score, Move move) {
+        this.numberOfEmptyTiles = numberOfEmptyTiles;
+        this.score = score;
+        this.move = move;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+}
