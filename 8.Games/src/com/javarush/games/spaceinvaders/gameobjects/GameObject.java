e5a40dde28299bef2bf6dd62dd0caf6b6848320c
Serg <s.ustin.92@mail.ru>
2023-11-14 15:43:13 +0300

    add SpaceInvadersGame 5/34

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/GameObject.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/GameObject.java
@@ -1,11 +1,30 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.*;
+
 public class GameObject {
     public double x;
     public double y;
+    public int[][] matrix;
+    public int width;
+    public int height;
 
     public GameObject(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+        this.width = matrix[0].length;
+        this.height = matrix.length;
+    }
+
+    public void draw(Game game) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                game.setCellValueEx((int) this.x + x, (int) this.y + y, Color.values()[matrix[y][x]], "");
+            }
+        }
     }
 }
