1fa3c8e7f276d5b676b624e0a3805e3086a6da02
Serg <s.ustin.92@mail.ru>
2023-11-14 16:06:27 +0300

    add SpaceInvadersGame 30/34

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
@@ -1,5 +1,6 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.games.spaceinvaders.Direction;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
 public class Boss extends EnemyShip {
@@ -18,4 +19,31 @@ public class Boss extends EnemyShip {
             super.nextFrame();
         }
     }
+
+    @Override
+    public Bullet fire() {
+        if (!isAlive) {
+            return null;
+        }
+
+        if (matrix == ShapeMatrix.BOSS_ANIMATION_FIRST) {
+            return new Bullet(x + 6, y + height, Direction.DOWN);
+        } else {
+            return new Bullet(x, y + height, Direction.DOWN);
+        }
+    }
+
+    @Override
+    public void kill() {
+        if (!isAlive) {
+            return;
+        }
+        isAlive = false;
+
+        this.setAnimatedView(
+                ShapeMatrix.KILL_BOSS_ANIMATION_FIRST,
+                ShapeMatrix.KILL_BOSS_ANIMATION_SECOND,
+                ShapeMatrix.KILL_BOSS_ANIMATION_THIRD);
+    }
+
 }
