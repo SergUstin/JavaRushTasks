a907858016275f4075f521144796aac94db1ca6c
Serg <s.ustin.92@mail.ru>
2023-11-14 16:14:08 +0300

    add Game2048_MVC 3/17

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,15 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private Tile[][] gameTiles;
+    private static final int FIELD_WIDTH = 4;
+
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
