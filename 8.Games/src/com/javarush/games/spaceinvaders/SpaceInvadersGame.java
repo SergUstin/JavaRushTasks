5bcf584bf26ec9e8674f5d35b11a80bd82ae3768
Serg <s.ustin.92@mail.ru>
2023-11-14 15:46:14 +0300

    add SpaceInvadersGame 8/34

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,6 +1,7 @@
 package com.javarush.games.spaceinvaders;
 
 import com.javarush.engine.cell.*;
+import com.javarush.games.spaceinvaders.gameobjects.EnemyFleet;
 import com.javarush.games.spaceinvaders.gameobjects.Star;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ public class SpaceInvadersGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private List<Star> stars;
+    private EnemyFleet enemyFleet;
 
     @Override
     public void initialize() {
@@ -17,13 +19,21 @@ public class SpaceInvadersGame extends Game {
         createGame();
     }
 
+    @Override
+    public void onTurn(int step) {
+        drawScene();
+    }
+
     private void createGame() {
+        enemyFleet = new EnemyFleet();
         createStars();
         drawScene();
+        setTurnTimer(40);
     }
 
     private void drawScene() {
         drawField();
+        enemyFleet.draw(this);
     }
 
     private void drawField() {
--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyFleet.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyFleet.java
@@ -1,5 +1,6 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.Game;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
 import java.util.ArrayList;
@@ -11,7 +12,22 @@ public class EnemyFleet {
     private static final int STEP = ShapeMatrix.ENEMY.length + 1;
     private List<EnemyShip> ships;
 
+    public EnemyFleet() {
+        createShips();
+    }
+
+    public void draw(Game game) {
+        for (EnemyShip ship : ships) {
+            ship.draw(game);
+        }
+    }
+
     private void createShips() {
         ships = new ArrayList<>();
+        for (int x = 0; x < COLUMNS_COUNT; x++) {
+            for (int y = 0; y < ROWS_COUNT; y++) {
+                ships.add(new EnemyShip(x * STEP, y * STEP + 12));
+            }
+        }
     }
 }
