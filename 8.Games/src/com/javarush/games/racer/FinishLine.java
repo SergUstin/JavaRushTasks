9e7651bf34a4a2766d50f6f7bbb3c9d180f89ea8
Serg <s.ustin.92@mail.ru>
2023-11-14 15:32:38 +0300

    add RacerGame 22/28

---
+++ 8.Games/src/com/javarush/games/racer/FinishLine.java
@@ -0,0 +1,21 @@
+package com.javarush.games.racer;
+
+public class FinishLine extends GameObject {
+
+    private boolean isVisible = false;
+
+    public FinishLine() {
+        super(RacerGame.ROADSIDE_WIDTH, -1 * ShapeMatrix.FINISH_LINE.length, ShapeMatrix.FINISH_LINE);
+    }
+
+    public void show() {
+        isVisible = true;
+    }
+
+    public void move(int boost) {
+        if (!isVisible) {
+            return;
+        }
+        y += boost;
+    }
+}
