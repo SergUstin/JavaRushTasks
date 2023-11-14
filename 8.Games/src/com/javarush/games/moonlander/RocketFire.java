130090f056777942db6c5457e3acfcf148f9c59b
Serg <s.ustin.92@mail.ru>
2023-11-14 15:02:18 +0300

    add MoonLanderGame 19/23

--- 8.Games/src/com/javarush/games/moonlander/RocketFire.java
+++ 8.Games/src/com/javarush/games/moonlander/RocketFire.java
@@ -1,5 +1,7 @@
 package com.javarush.games.moonlander;
 
+import com.javarush.engine.cell.*;
+
 import java.util.List;
 
 public class RocketFire extends GameObject {
@@ -14,4 +16,20 @@ public class RocketFire extends GameObject {
         this.isVisible = false;
     }
 
+    private void nextFrame() {
+        frameIndex++;
+        if (frameIndex >= frames.size()) {
+            frameIndex = 0;
+        }
+        matrix = frames.get(frameIndex);
+    }
+
+    @Override
+    public void draw(Game game) {
+        if (!isVisible) {
+            return;
+        }
+        nextFrame();
+        super.draw(game);
+    }
 }
