2d0e11422bf82d163a7349cba06d05dd0d992618
Serg <s.ustin.92@mail.ru>
2023-11-21 16:49:06 +0300

    add Space 14/18

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -1,31 +1,62 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Класс для космического корабля
+ */
 public class SpaceShip extends BaseObject {
+    //картинка корабля для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
 
-    private double dx;
+    //вектор движения (-1 влево,+1 вправо)
+    private double dx = 0;
 
     public SpaceShip(double x, double y) {
         super(x, y, 3);
     }
 
+    /**
+     * Устанавливаем вектор движения влево
+     */
     public void moveLeft() {
         dx = -1;
     }
 
+    /**
+     * Устанавливаем вектор движения вправо
+     */
     public void moveRight() {
         dx = 1;
     }
 
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'M');
     }
 
+    /**
+     * Двигаем себя на один ход.
+     * Проверяем столкновение с границами.
+     */
     @Override
     public void move() {
         x = x + dx;
+
         checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
     }
 
+    /**
+     * Стреляем.
+     * Создаем две ракеты: слева и справа от корабля.
+     */
     public void fire() {
         Space.game.getRockets().add(new Rocket(x - 2, y));
         Space.game.getRockets().add(new Rocket(x + 2, y));
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -2,7 +2,40 @@ package com.javarush.task.task25.task2515;
 
 public class Ufo extends BaseObject {
 
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+    //картинка корабля для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    @Override
+    public void move() {
+        double dx = Math.random() * 2 - 1;
+        double dy = Math.random() * 2 - 1;
+
+        x += dx;
+        y += dy;
+
+        checkBorders(radius, Space.game.getWidth() - radius + 1, radius - 1, Space.game.getHeight() / 2 - 1);
+
+        int random10 = (int) (Math.random() * 10);
+        if (random10 == 0)
+            fire();
+    }
+
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y + 3));
     }
 }
