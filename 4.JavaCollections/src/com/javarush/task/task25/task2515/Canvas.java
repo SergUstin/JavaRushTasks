20c4f1b771093344e5adc33c47f50a76e48a26af
Serg <s.ustin.92@mail.ru>
2023-11-21 16:47:09 +0300

    add Space 9/18

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task25.task2515;
 
-public class Canvas {
 
+public class Canvas {
     private int width;
     private int height;
     private char[][] matrix;
@@ -9,15 +9,11 @@ public class Canvas {
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        matrix = new char[height][width];
+        this.matrix = new char[height + 2][width + 2];
     }
 
-    public void setPoint(double x, double y, char c) {
-        int xRounded = (int) Math.round(x);
-        int yRounded = (int) Math.round(y);
-        if (xRounded >= 0 && xRounded < matrix[0].length && yRounded >= 0 && yRounded < matrix.length) {
-            matrix[yRounded][xRounded] = c;
-        }
+    public void clear() {
+        this.matrix = new char[height + 2][width + 2];
     }
 
     public void drawMatrix(double x, double y, int[][] matrix, char c) {
@@ -30,15 +26,19 @@ public class Canvas {
         }
     }
 
-    public void clear() {
-        this.matrix = new char[height][width];
+    public void setPoint(double x, double y, char c) {
+        int xRounded = (int) Math.round(x);
+        int yRounded = (int) Math.round(y);
+        if (xRounded >= 0 && xRounded < matrix[0].length && yRounded >= 0 && yRounded < matrix.length) {
+            matrix[yRounded][xRounded] = c;
+        }
     }
 
     public void print() {
         System.out.println();
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
                 System.out.print(" ");
                 System.out.print(matrix[i][j]);
                 System.out.print(" ");
