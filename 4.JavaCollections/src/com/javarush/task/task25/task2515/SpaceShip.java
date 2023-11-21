2ddbd48d5765cf0d01f22d7087b987a81108c562
Serg <s.ustin.92@mail.ru>
2023-11-21 16:48:08 +0300

    add Space 13/18

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -15,4 +15,19 @@ public class SpaceShip extends BaseObject {
     public void moveRight() {
         dx = 1;
     }
+
+    @Override
+    public void draw(Canvas canvas) {
+    }
+
+    @Override
+    public void move() {
+        x = x + dx;
+        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
+    }
+
+    public void fire() {
+        Space.game.getRockets().add(new Rocket(x - 2, y));
+        Space.game.getRockets().add(new Rocket(x + 2, y));
+    }
 }
