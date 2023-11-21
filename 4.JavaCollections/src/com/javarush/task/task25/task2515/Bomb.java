b88380ecd14bee355161a53ab630c7e785c6cd47
Serg <s.ustin.92@mail.ru>
2023-11-21 16:47:36 +0300

    add Space 11/18

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -1,16 +1,24 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Класс для бомбы.
+ */
 public class Bomb extends BaseObject {
-
     public Bomb(double x, double y) {
         super(x, y, 1);
     }
 
+    /**
+     * Отрисовываем себя на холсте.
+     */
     @Override
     public void draw(Canvas canvas) {
         canvas.setPoint(x, y, 'B');
     }
 
+    /**
+     * Двигаем себя вниз на один ход.
+     */
     @Override
     public void move() {
         y++;
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -2,7 +2,17 @@ package com.javarush.task.task25.task2515;
 
 public class Rocket extends BaseObject {
 
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    @Override
+    public void move() {
+        y--;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
     }
 }
