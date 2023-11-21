1e3a1eb2874bb1a45a3b16f426074abb430a0461
Serg <s.ustin.92@mail.ru>
2023-11-21 16:55:33 +0300

    add AmigoSet 5/6

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 
@@ -58,6 +60,42 @@ public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Se
             return newSet;
         } catch (Exception e) {
             throw new InternalError();
+        }
+    }
+
+    private void writeObject(ObjectOutputStream s) throws java.io.IOException {
+        // Write out any hidden serialization magic
+        s.defaultWriteObject();
+
+        // Write out HashMap capacity and load factor
+        s.writeInt(HashMapReflectionHelper.<Integer>callHiddenMethod(map, "capacity"));
+        s.writeFloat(HashMapReflectionHelper.<Float>callHiddenMethod(map, "loadFactor"));
+
+        // Write out size
+        s.writeInt(map.size());
+
+        // Write out all elements in the proper order.
+        for (E e : map.keySet())
+            s.writeObject(e);
+    }
+
+
+    private void readObject(ObjectInputStream s) throws java.io.IOException, ClassNotFoundException {
+        // Read in any hidden serialization magic
+        s.defaultReadObject();
+
+        // Read in HashMap capacity and load factor and create backing HashMap
+        int capacity = s.readInt();
+        float loadFactor = s.readFloat();
+        map = new HashMap<>(capacity, loadFactor);
+
+        // Read in size
+        int size = s.readInt();
+
+        // Read in all elements in the proper order.
+        for (int i = 0; i < size; i++) {
+            E e = (E) s.readObject();
+            map.put(e, PRESENT);
         }
     }
 }
