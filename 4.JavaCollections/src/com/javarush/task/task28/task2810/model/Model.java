c3c29ac3844e1a08fa3da386b49e29ad321f9b69
Serg <s.ustin.92@mail.ru>
2023-11-28 17:38:35 +0300

    add Aggregator 10/17

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Model.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task28.task2810.model;
+
+import com.javarush.task.task28.task2810.view.View;
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+
+    private View view;
+    private Provider[] providers;
+
+    public Model(View view, Provider... providers) {
+        if (view == null || providers == null || providers.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        this.view = view;
+        this.providers = providers;
+    }
+
+    public void selectCity(String city) {
+        List<Vacancy> vacancies = new ArrayList<>();
+        for (Provider provider : providers) {
+            vacancies.addAll(provider.getJavaVacancies(city));
+        }
+        view.update(vacancies);
+    }
+}
