64751fd655b5a6ef1337f371a709ab1b3de71768
Serg <s.ustin.92@mail.ru>
2023-11-28 17:34:14 +0300

    add Aggregator 3/17

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
@@ -1,11 +1,19 @@
 package com.javarush.task.task28.task2810.model;
 
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.List;
+
 public class Provider {
 
     private Strategy strategy;
 
     public Provider(Strategy strategy) {
         this.strategy = strategy;
+    }
+
+    public List<Vacancy> getJavaVacancies(String searchString) {
+        return null;
     }
 
     public void setStrategy(Strategy strategy) {
