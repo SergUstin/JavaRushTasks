115800fe60130c13acae5d58736ec309ecff21f0
Serg <s.ustin.92@mail.ru>
2023-11-28 17:36:48 +0300

    add Aggregator 8/17

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -3,6 +3,8 @@ package com.javarush.task.task28.task2810.model;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,14 +16,47 @@ public class HHStrategy implements Strategy {
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
+        List<Vacancy> allVacancies = new ArrayList<>();
+
+        int page = 0;
         try {
-            Document document = Jsoup.connect(String.format(URL_FORMAT, "MyTown", 0))
-                    .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
-                    .referrer("https://hh.ru/")
-                    .get();
+            do {
+                Document doc = getDocument(searchString, page);
+
+                Elements vacanciesHtmlList = doc.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy");
+
+                if (vacanciesHtmlList.isEmpty()) break;
+
+                for (Element element : vacanciesHtmlList) {
+                    Elements links = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title");
+                    Elements locations = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-address");
+                    Elements companyName = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-employer");
+                    Elements salary = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-compensation");
+
+                    Vacancy vacancy = new Vacancy();
+                    vacancy.setSiteName("hh.ru");
+                    vacancy.setTitle(links.get(0).text());
+                    vacancy.setUrl(links.get(0).attr("href"));
+                    vacancy.setCity(locations.get(0).text());
+                    vacancy.setCompanyName(companyName.get(0).text());
+                    vacancy.setSalary(salary.size() > 0 ? salary.get(0).text() : "");
+
+                    allVacancies.add(vacancy);
+                }
+
+                page++;
+            } while (true);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new ArrayList<>();
+
+        return allVacancies;
+    }
+
+    protected Document getDocument(String searchString, int page) throws IOException {
+        return Jsoup.connect(String.format(URL_FORMAT, searchString, page))
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+                .referrer("https://hh.ru/")
+                .get();
     }
 }
