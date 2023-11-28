ab388154d29f5a8cbb21a50c32d700eaaade11dc
Serg <s.ustin.92@mail.ru>
2023-11-28 18:04:03 +0300

    add Sokoban 15/17

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -25,6 +25,19 @@ public class Model {
     }
 
     public void move(Direction direction) {
+        if (checkWallCollision(gameObjects.getPlayer(), direction)) {
+            return;
+        }
+
+        if (checkBoxCollisionAndMoveIfAvailable(direction)) {
+            return;
+        }
+
+        int dx = direction == Direction.LEFT ? -FIELD_CELL_SIZE : (direction == Direction.RIGHT ? FIELD_CELL_SIZE : 0);
+        int dy = direction == Direction.UP ? -FIELD_CELL_SIZE : (direction == Direction.DOWN ? FIELD_CELL_SIZE : 0);
+        gameObjects.getPlayer().move(dx, dy);
+
+        checkCompletion();
     }
 
     public void restart() {
@@ -34,6 +47,54 @@ public class Model {
     public void startNextLevel() {
         currentLevel++;
         restartLevel(currentLevel);
+    }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+        for (Wall wall : gameObjects.getWalls()) {
+            if (gameObject.isCollision(wall, direction)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean checkBoxCollisionAndMoveIfAvailable(Direction direction) {
+        for (Box box : gameObjects.getBoxes()) {
+            if (gameObjects.getPlayer().isCollision(box, direction)) {
+                for (Box item : gameObjects.getBoxes()) {
+                    if (!box.equals(item)) {
+                        if (box.isCollision(item, direction)) {
+                            return true;
+                        }
+                    }
+                    if (checkWallCollision(box, direction)) {
+                        return true;
+                    }
+                }
+                int dx = direction == Direction.LEFT ? -FIELD_CELL_SIZE : (direction == Direction.RIGHT ? FIELD_CELL_SIZE : 0);
+                int dy = direction == Direction.UP ? -FIELD_CELL_SIZE : (direction == Direction.DOWN ? FIELD_CELL_SIZE : 0);
+                box.move(dx, dy);
+            }
+        }
+        return false;
+    }
+
+    public void checkCompletion() {
+        int numberOfHomes = gameObjects.getHomes().size();
+        int numberOfHomesWithBox = 0;
+
+        for (Home home : gameObjects.getHomes()) {
+            for (Box box : gameObjects.getBoxes()) {
+                if (box.getX() == home.getX() && box.getY() == home.getY()) {
+                    numberOfHomesWithBox++;
+                }
+            }
+        }
+
+        if (numberOfHomesWithBox == numberOfHomes) {
+            eventListener.levelCompleted(currentLevel);
+        }
     }
 
     public void restartLevel(int level) {
