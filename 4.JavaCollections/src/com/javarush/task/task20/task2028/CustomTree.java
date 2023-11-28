5c636c659362edf921580ec1a4bc26e0e38a3efc
Serg <s.ustin.92@mail.ru>
2023-11-28 16:59:02 +0300

    add CustomTree 2/5

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -2,19 +2,47 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
-
-/* 
-Построй дерево(1)
-*/
+import java.util.Collection;
+import java.util.List;
 
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
     @Override
     public String get(int index) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public int size() {
         return 0;
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw new UnsupportedOperationException();
     }
 }
