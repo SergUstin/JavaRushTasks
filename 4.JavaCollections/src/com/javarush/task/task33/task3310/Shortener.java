18e25c739333d2ca8604baa457dc6554e9acf114
Serg <s.ustin.92@mail.ru>
2023-11-28 17:11:48 +0300

    add Shortener 3/16

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,11 +1,26 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 public class Shortener {
-    public Long getId(String string) {
-        return null;
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id) {
-        return null;
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string))
+            return storageStrategy.getKey(string);
+
+        lastId++;
+        storageStrategy.put(lastId, string);
+        return lastId;
+    }
+
+    public synchronized String getString(Long id) {
+        return storageStrategy.getValue(id);
     }
 }
+
