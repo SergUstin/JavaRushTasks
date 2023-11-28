e7bcf308bb441535c11872d20e0e59f1f0da5470
Serg <s.ustin.92@mail.ru>
2023-11-28 17:45:57 +0300

    add CashMachine 3/15

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task26.task2613;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CurrencyManipulator {
+    private String currencyCode;
+    private Map<Integer, Integer> denominations;
+
+    public CurrencyManipulator(String currencyCode) {
+        this.currencyCode = currencyCode;
+        this.denominations = new TreeMap<>();
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulatorFactory.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task26.task2613;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CurrencyManipulatorFactory {
+    private static Map<String, CurrencyManipulator> map = new HashMap<>();
+
+    private CurrencyManipulatorFactory() {
+    }
+
+    public static CurrencyManipulator getManipulatorByCurrencyCode(String currencyCode) {
+        currencyCode = currencyCode.toUpperCase();
+        if (!map.containsKey(currencyCode)) {
+            CurrencyManipulator manipulator = new CurrencyManipulator(currencyCode);
+            map.put(manipulator.getCurrencyCode(), manipulator);
+        }
+        return map.get(currencyCode);
+    }
+}
