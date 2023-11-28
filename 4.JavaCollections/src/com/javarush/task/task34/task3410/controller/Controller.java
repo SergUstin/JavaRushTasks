0cf954cb41bc998ab8d6c70df16862dc468c7c9a
Serg <s.ustin.92@mail.ru>
2023-11-28 17:56:02 +0300

    add Sokoban 1/17

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task34.task3410.controller;
+
+import com.javarush.task.task34.task3410.model.Model;
+import com.javarush.task.task34.task3410.view.View;
+
+public class Controller {
+    private View view;
+    private Model model;
+
+    public Controller() {
+        model = new Model();
+        view = new View(this);
+    }
+
+    public static void main(String[] args) {
+    }
+}
