a131ced55aad4c0d014f0ffd081fa5ec073e8185
Serg <s.ustin.92@mail.ru>
2023-11-28 18:00:24 +0300

    add Sokoban 8/17

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/GameObjects.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GameObjects {
+    private Set<Wall> walls;
+    private Set<Box> boxes;
+    private Set<Home> homes;
+    private Player player;
+
+    public GameObjects(Set<Wall> walls, Set<Box> boxes, Set<Home> homes, Player player) {
+        this.walls = walls;
+        this.boxes = boxes;
+        this.homes = homes;
+        this.player = player;
+    }
+
+    public Set<Wall> getWalls() {
+        return walls;
+    }
+
+    public Set<Box> getBoxes() {
+        return boxes;
+    }
+
+    public Set<Home> getHomes() {
+        return homes;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Set<GameObject> getAll() {
+        Set<GameObject> gameObjects = new HashSet<>();
+        gameObjects.addAll(walls);
+        gameObjects.addAll(homes);
+        gameObjects.addAll(boxes);
+        gameObjects.add(player);
+        return gameObjects;
+    }
+}
