fafce7bda1780a098498647c2fcd80587f6025a2
Serg <s.ustin.92@mail.ru>
2023-11-28 17:41:44 +0300

    add Aggregator 14/17

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -3,6 +3,9 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
@@ -35,6 +38,11 @@ public class HtmlView implements View {
     }
 
     private void updateFile(String content) {
-
+        File file = new File(filePath);
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            outputStream.write(content.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
