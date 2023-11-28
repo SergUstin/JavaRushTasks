2afd8009b4536d1fcdaebd50824a7ddc11eaf25d
Serg <s.ustin.92@mail.ru>
2023-11-28 18:02:19 +0300

    add Sokoban 11/17

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -2,12 +2,42 @@ package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
 
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
 public class Model {
     public static final int FIELD_CELL_SIZE = 20;
 
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
     private EventListener eventListener;
+    LevelLoader levelLoader;
+
+    public Model() {
+        try {
+            levelLoader = new LevelLoader(Paths.get(getClass().getResource("../res/levels.txt").toURI()));
+        } catch (URISyntaxException e) {
+        }
+    }
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel++;
+        restartLevel(currentLevel);
+    }
+
+    public void restartLevel(int level) {
+        gameObjects = levelLoader.getLevel(level);
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
     }
 }
