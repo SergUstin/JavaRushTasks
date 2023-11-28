8e53d1697fb3474b28c75034a31b7fad673ea1aa
Serg <s.ustin.92@mail.ru>
2023-11-28 17:50:50 +0300

    add CashMachine 10/15

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task26.task2613;
 
-import java.util.Map;
-import java.util.TreeMap;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.*;
 
 public class CurrencyManipulator {
     private String currencyCode;
@@ -36,4 +37,50 @@ public class CurrencyManipulator {
         return !denominations.isEmpty();
     }
 
+    public boolean isAmountAvailable(int expectedAmount) {
+        return expectedAmount <= getTotalAmount();
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        int sum = expectedAmount;
+        HashMap<Integer, Integer> copyDenominations = new HashMap<>(denominations);
+
+        ArrayList<Integer> keys = new ArrayList<>(this.denominations.keySet());
+
+        Collections.sort(keys);
+        Collections.reverse(keys);
+
+        TreeMap<Integer, Integer> resultMap = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+
+        for (Integer denomination : keys) {
+            final int key = denomination;
+            int value = copyDenominations.get(key);
+            while (true) {
+                if (sum < key || value == 0) {
+                    copyDenominations.put(key, value);
+                    break;
+                }
+                sum -= key;
+                value--;
+
+                if (resultMap.containsKey(key))
+                    resultMap.put(key, resultMap.get(key) + 1);
+                else
+                    resultMap.put(key, 1);
+            }
+        }
+
+        if (sum > 0)
+            throw new NotEnoughMoneyException();
+        else {
+            this.denominations.clear();
+            this.denominations.putAll(copyDenominations);
+        }
+        return resultMap;
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,9 +1,51 @@
 package com.javarush.task.task26.task2613.command;
 
 
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Map;
+
 class WithdrawCommand implements Command {
 
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException {
+        ConsoleHelper.writeMessage("Withdrawing...");
+
+        String currencyCode = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+
+        while (true) {
+            try {
+                ConsoleHelper.writeMessage("Please specify integer amount for withdrawing.");
+                String s = ConsoleHelper.readString();
+                if (s == null) {
+                    ConsoleHelper.writeMessage("Please specify valid positive integer amount for withdrawing.");
+                } else {
+                    try {
+                        int amount = Integer.parseInt(s);
+                        boolean isAmountAvailable = manipulator.isAmountAvailable(amount);
+                        if (isAmountAvailable) {
+                            Map<Integer, Integer> denominations = manipulator.withdrawAmount(amount);
+                            for (Integer item : denominations.keySet()) {
+                                ConsoleHelper.writeMessage("\t" + item + " - " + denominations.get(item));
+                            }
+
+                            ConsoleHelper.writeMessage(String.format("%d %s was withdrawn successfully", amount, currencyCode));
+                            break;
+                        } else {
+                            ConsoleHelper.writeMessage("Not enough money on your account, please try again");
+                        }
+                    } catch (NumberFormatException e) {
+                        ConsoleHelper.writeMessage("Please specify valid positive integer amount for withdrawing.");
+                    }
+                }
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage("Exact amount is not available");
+            }
+        }
     }
 }
