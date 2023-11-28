a89e96630eb00a326228ea318f80b333e1403db3
Serg <s.ustin.92@mail.ru>
2023-11-28 16:39:17 +0300

    add Restaurant 4/23

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
@@ -5,7 +5,7 @@ import com.javarush.task.task27.task2712.ConsoleHelper;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Cook implements Observer {
+public class Cook extends Observable implements Observer {
     private final String name;
 
     public Cook(String name) {
@@ -15,6 +15,8 @@ public class Cook implements Observer {
     @Override
     public void update(Observable o, Object order) {
         ConsoleHelper.writeMessage("Start cooking - " + order);
+        setChanged();
+        notifyObservers(order);
     }
 
     @Override
