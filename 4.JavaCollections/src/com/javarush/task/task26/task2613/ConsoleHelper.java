c818c125d4160716aaf83e1e4b8b45986d7a135e
Serg <s.ustin.92@mail.ru>
2023-11-28 17:45:14 +0300

    add CashMachine 2/15

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,5 +1,24 @@
 package com.javarush.task.task26.task2613;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        try {
+            String text = bis.readLine();
+            return text;
+        } catch (IOException ignored) {
+            /* NOP */
+        }
+        return null;
+    }
 
 }
