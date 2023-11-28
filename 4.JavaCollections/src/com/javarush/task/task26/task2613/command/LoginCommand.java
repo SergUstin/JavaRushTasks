df2b914c2ab82c6dcb67899ed8c95f91c31dccd6
Serg <s.ustin.92@mail.ru>
2023-11-28 17:51:55 +0300

    add CashMachine 12/15

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
@@ -1,12 +1,14 @@
 package com.javarush.task.task26.task2613.command;
 
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 
+import java.util.ResourceBundle;
+
 class LoginCommand implements Command {
-    private String validCreditCard = "123456789012";
-    private String validPin = "1234";
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.verifiedCards");
 
     @Override
     public void execute() throws InterruptOperationException {
@@ -21,7 +23,7 @@ class LoginCommand implements Command {
                 ConsoleHelper.writeMessage("Please specify valid credit card number - 12 digits, pin code - 4 digits.");
             } else {
                 try {
-                    if (creditCardNumber.equals(validCreditCard) && pinStr.equals(validPin)) {
+                    if (validCreditCards.containsKey(creditCardNumber) && pinStr.equals(validCreditCards.getString(creditCardNumber))) {
                         ConsoleHelper.writeMessage(String.format("Credit card [%s] is verified successfully!", creditCardNumber));
                         break;
                     } else {
