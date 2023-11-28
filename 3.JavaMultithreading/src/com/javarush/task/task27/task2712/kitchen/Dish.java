4cae76bcc8625580924cf01c25f678734d004067
Serg <s.ustin.92@mail.ru>
2023-11-28 16:36:25 +0300

    add Restaurant 1/23

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Dish.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+public enum Dish {
+    FISH,
+    STEAK,
+    SOUP,
+    JUICE,
+    WATER;
+
+    public static String allDishesToString() {
+        String result = "";
+
+        for (Dish dish : Dish.values()) {
+            if ("".equals(result)) {
+                result += dish.name();
+            } else {
+                result += ", " + dish.name();
+            }
+        }
+        return result;
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+import com.javarush.task.task27.task2712.Tablet;
+
+import java.util.List;
+
+public class Order {
+    private final Tablet tablet;
+    protected List<Dish> dishes;
+
+    public Order(Tablet tablet) {
+        this.tablet = tablet;
+    }
+}
