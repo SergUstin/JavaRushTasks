82f4827a5fff4e679afc1488e8cb12c14851377c
Serg <s.ustin.92@mail.ru>
2023-11-28 17:01:21 +0300

    add CustomTree 5/5

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -13,5 +13,17 @@ public class Solution {
         System.out.println("The list size is " + list.size());
         System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
         System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
+
+        list.remove("3");
+        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("8"));
+
+        list.add("16");
+        System.out.println("The expected parent is 9. The actual parent is " + ((CustomTree) list).getParent("16"));
+
+        list.remove("4");
+        list.remove("5");
+        list.remove("6");
+        System.out.println("Expected: true. Actual: " + list.add("20"));
+        System.out.println("The expected parent is 1. The actual parent is " + ((CustomTree) list).getParent("20"));
     }
 }
