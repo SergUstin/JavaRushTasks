a8a6a1f29ade13395330cdcaf8e00c6bd919fde7
Serg <s.ustin.92@mail.ru>
2023-11-28 17:39:01 +0300

    add Aggregator 11/17

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,13 +1,9 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.HHStrategy;
-import com.javarush.task.task28.task2810.model.Provider;
 
 public class Aggregator {
 
     public static void main(String[] args) {
-        Provider provider = new Provider(new HHStrategy());
-        Controller controller = new Controller(provider);
-        controller.scan();
+
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,33 +1,17 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.task.task28.task2810.model.Model;
 
 public class Controller {
 
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers.length == 0) throw new IllegalArgumentException();
-        this.providers = providers;
+    public Controller(Model model) {
+        if (model == null) throw new IllegalArgumentException();
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        List<Vacancy> vacancies = new ArrayList<>();
-        for (Provider provider : providers) {
-            vacancies.addAll(provider.getJavaVacancies("MyTown"));
-        }
-        System.out.println(vacancies.size());
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
