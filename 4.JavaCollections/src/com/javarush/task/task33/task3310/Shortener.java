640c090030ee99d2e873c3d43d88062c535dff14
Serg <s.ustin.92@mail.ru>
2023-11-28 17:10:45 +0300

    add Shortener 1/16

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task33.task3310;
+
+public class Shortener {
+    public Long getId(String string) {
+        return null;
+    }
+
+    public String getString(Long id) {
+        return null;
+    }
+}
