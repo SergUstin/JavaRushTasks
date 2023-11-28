8bab80915faec0b68a6a509aa5203bb8b8ff5719
Serg <s.ustin.92@mail.ru>
2023-11-28 18:03:43 +0300

    add Sokoban 14/17

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,10 +1,13 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.Direction;
 import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Set;
 
 public class Field extends JPanel {
@@ -13,6 +16,9 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+
+        addKeyListener(new KeyHandler());
+        setFocusable(true);
     }
 
     public void setEventListener(EventListener eventListener) {
@@ -28,6 +34,24 @@ public class Field extends JPanel {
 
         for (GameObject gameObject : gameObjects) {
             gameObject.draw(g);
+        }
+    }
+
+    public class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            if (key == KeyEvent.VK_LEFT) {
+                eventListener.move(Direction.LEFT);
+            } else if (key == KeyEvent.VK_RIGHT) {
+                eventListener.move(Direction.RIGHT);
+            } else if (key == KeyEvent.VK_UP) {
+                eventListener.move(Direction.UP);
+            } else if (key == KeyEvent.VK_DOWN) {
+                eventListener.move(Direction.DOWN);
+            } else if (key == KeyEvent.VK_R) {
+                eventListener.restart();
+            }
         }
     }
 }
