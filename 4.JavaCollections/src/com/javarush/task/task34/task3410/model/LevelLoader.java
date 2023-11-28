d534efd9080ebd9f9415a90baa0798e86c2ec92b
Serg <s.ustin.92@mail.ru>
2023-11-28 18:04:33 +0300

    add Sokoban 16/17

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/LevelLoader.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/LevelLoader.java
@@ -1,31 +1,87 @@
 package com.javarush.task.task34.task3410.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
 public class LevelLoader {
+    private Path levels;
+
     public LevelLoader(Path levels) {
+        this.levels = levels;
     }
 
     public GameObjects getLevel(int level) {
-
-
         Set<Wall> walls = new HashSet<>();
         Set<Box> boxes = new HashSet<>();
         Set<Home> homes = new HashSet<>();
-        Player player;
+        Player player = null;
 
-        int x = Model.FIELD_CELL_SIZE / 2;
-        int y = Model.FIELD_CELL_SIZE / 2;
+        int loopLevel;
+        if (level > 60) {
+            loopLevel = level % 60;
+        } else {
+            loopLevel = level;
+        }
 
-        walls.add(new Wall(x + 0 * Model.FIELD_CELL_SIZE, y));
-        walls.add(new Wall(x + 1 * Model.FIELD_CELL_SIZE, y));
-        walls.add(new Wall(x + 2 * Model.FIELD_CELL_SIZE, y));
+        try (BufferedReader reader = new BufferedReader(new FileReader(levels.toFile()))) {
+            int readLevel = 0;
+            int x;
+            int y = Model.FIELD_CELL_SIZE / 2;
+            boolean isLevelMap = false;
 
-        boxes.add(new Box(x + 0 * Model.FIELD_CELL_SIZE, y + Model.FIELD_CELL_SIZE));
-        homes.add(new Home(x + 1 * Model.FIELD_CELL_SIZE, y + Model.FIELD_CELL_SIZE));
-        player = new Player(x + 2 * Model.FIELD_CELL_SIZE, y + Model.FIELD_CELL_SIZE);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("Maze:")) {
+                    readLevel = Integer.valueOf(line.split(" ")[1]);
+                    continue;
+                }
+                if (readLevel == loopLevel) {
+                    if (line.length() == 0) {
+                        boolean isEnd = isLevelMap;
+
+                        isLevelMap = !isLevelMap;
+
+                        if (isEnd && !isLevelMap) {
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+                    if (isLevelMap) {
+                        x = Model.FIELD_CELL_SIZE / 2;
+
+                        char[] chars = line.toCharArray();
+                        for (char c : chars) {
+                            switch (c) {
+                                case 'X':
+                                    walls.add(new Wall(x, y));
+                                    break;
+                                case '*':
+                                    boxes.add(new Box(x, y));
+                                    break;
+                                case '.':
+                                    homes.add(new Home(x, y));
+                                    break;
+                                case '&':
+                                    boxes.add(new Box(x, y));
+                                    homes.add(new Home(x, y));
+                                    break;
+                                case '@':
+                                    player = new Player(x, y);
+                            }
+                            x += Model.FIELD_CELL_SIZE;
+                        }
+                        y += Model.FIELD_CELL_SIZE;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return new GameObjects(walls, boxes, homes, player);
     }
