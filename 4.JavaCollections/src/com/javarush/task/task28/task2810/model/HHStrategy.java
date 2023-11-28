7a8a388b908bde6369706c7d989208c259f094e3
Serg <s.ustin.92@mail.ru>
2023-11-28 17:35:30 +0300

    add Aggregator 6/17

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +14,11 @@ public class HHStrategy implements Strategy {
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
+        try {
+            Document document = Jsoup.connect(String.format(URL_FORMAT, "MyTown", 0)).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new ArrayList<>();
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
@@ -9,6 +9,33 @@ public class Vacancy {
     private String siteName;
     private String url;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vacancy vacancy = (Vacancy) o;
+
+        if (title != null ? !title.equals(vacancy.title) : vacancy.title != null) return false;
+        if (salary != null ? !salary.equals(vacancy.salary) : vacancy.salary != null) return false;
+        if (city != null ? !city.equals(vacancy.city) : vacancy.city != null) return false;
+        if (companyName != null ? !companyName.equals(vacancy.companyName) : vacancy.companyName != null) return false;
+        if (siteName != null ? !siteName.equals(vacancy.siteName) : vacancy.siteName != null) return false;
+        return url != null ? url.equals(vacancy.url) : vacancy.url == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (salary != null ? salary.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (companyName != null ? companyName.hashCode() : 0);
+        result = 31 * result + (siteName != null ? siteName.hashCode() : 0);
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
+    }
+
     public String getTitle() {
         return title;
     }
