2275b5e460b8efc2dcb03d5e444e4aa435d43705
Serg <s.ustin.92@mail.ru>
2023-11-28 16:58:36 +0300

    add CustomTree 1/5

---
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task20.task2028;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+
+/* 
+Построй дерево(1)
+*/
+
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task20.task2028;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+    }
+}
