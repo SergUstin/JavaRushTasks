1ac02141714d343d666c0b6f52644bbe0e1c080c
Serg <s.ustin.92@mail.ru>
2023-11-28 16:50:41 +0300

    add Restaurant 16/23

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/DirectorTablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/DirectorTablet.java
@@ -1,13 +1,46 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.statistic.StatisticManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
 public class DirectorTablet {
 
     public void printAdvertisementProfit() {
+        StatisticManager statisticManager = StatisticManager.getInstance();
+        Map<String, Long> profitMap = statisticManager.getProfitMap();
+        ArrayList<String> list = new ArrayList(profitMap.keySet());
+        Collections.sort(list);
+
+        for (String key : list) {
+            double amount = 1.0 * profitMap.get(key) / 100;
+            System.out.println(key + " - " + String.format(Locale.ENGLISH, "%.2f", amount));
+        }
     }
 
     public void printCookWorkloading() {
+        StatisticManager statisticManager = StatisticManager.getInstance();
+        Map<String, Map<String, Integer>> cookWorkloadingMap = statisticManager.getCookWorkloadingMap();
+        ArrayList<String> list = new ArrayList(cookWorkloadingMap.keySet());
+        Collections.sort(list);
 
+        for (String key : list) {
+            Map<String, Integer> cookMap = cookWorkloadingMap.get(key);
+            System.out.println(key);
+
+            ArrayList<String> cookNames = new ArrayList(cookMap.keySet());
+            Collections.sort(cookNames);
+            for (String cookName : cookNames) {
+                System.out.println(cookName + " - " + ((cookMap.get(cookName) + 59) / 60) + " min");
+            }
+
+            System.out.println();
+        }
     }
+
 
     public void printActiveVideoSet() {
 
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.statistic.event.CookedOrderEventDataRow;
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
 import com.javarush.task.task27.task2712.statistic.event.EventType;
+import com.javarush.task.task27.task2712.statistic.event.VideoSelectedEventDataRow;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class StatisticManager {
@@ -37,7 +40,10 @@ public class StatisticManager {
         }
 
         private List<EventDataRow> get(EventType type) {
-            return null;
+            if (!this.storage.containsKey(type))
+                throw new UnsupportedOperationException();
+
+            return this.storage.get(type);
         }
     }
 
@@ -50,10 +56,45 @@ public class StatisticManager {
     }
 
     public Map<String, Long> getProfitMap() {
-        return null;
+        Map<String, Long> res = new HashMap();
+        List<EventDataRow> rows = statisticStorage.get(EventType.SELECTED_VIDEOS);
+        SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+        long total = 0l;
+        for (EventDataRow row : rows) {
+            VideoSelectedEventDataRow dataRow = (VideoSelectedEventDataRow) row;
+            String date = format.format(dataRow.getDate());
+            if (!res.containsKey(date)) {
+                res.put(date, 0l);
+            }
+            total += dataRow.getAmount();
+            res.put(date, res.get(date) + dataRow.getAmount());
+        }
+
+        res.put("Total", total);
+
+        return res;
     }
 
     public Map<String, Map<String, Integer>> getCookWorkloadingMap() {
-        return null;
+        Map<String, Map<String, Integer>> res = new HashMap(); //name, time
+        List<EventDataRow> rows = statisticStorage.get(EventType.COOKED_ORDER);
+        SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+        for (EventDataRow row : rows) {
+            CookedOrderEventDataRow dataRow = (CookedOrderEventDataRow) row;
+            String date = format.format(dataRow.getDate());
+            if (!res.containsKey(date)) {
+                res.put(date, new HashMap<String, Integer>());
+            }
+            Map<String, Integer> cookMap = res.get(date);
+            String cookName = dataRow.getCookName();
+            if (!cookMap.containsKey(cookName)) {
+                cookMap.put(cookName, 0);
+            }
+
+            Integer totalTime = cookMap.get(cookName);
+            cookMap.put(cookName, totalTime + dataRow.getTime());
+        }
+
+        return res;
     }
 }
