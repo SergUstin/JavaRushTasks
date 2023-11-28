ceac99bbf93c37587d693eb6903dacf9308ca435
Serg <s.ustin.92@mail.ru>
2023-11-28 17:39:18 +0300

    add Aggregator 12/17

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,9 +1,20 @@
 package com.javarush.task.task28.task2810;
 
 
+import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Model;
+import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.view.HtmlView;
+
 public class Aggregator {
 
     public static void main(String[] args) {
+        HtmlView view = new HtmlView();
+        Model model = new Model(view, new Provider(new HHStrategy()));
+        Controller controller = new Controller(model);
 
+        view.setController(controller);
+
+        view.userCitySelectEmulationMethod();
     }
 }
